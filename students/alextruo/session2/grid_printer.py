8a6dff7f25db7213eb7a238b45934b3cc304d9a3
Alex Truong <atruong012@gmail.com>
2016-10-04 19:54:21 -0700

    Added my grid file with print statements.

---
+++ students/alextruo/session2/grid_printer.py
@@ -0,0 +1,25 @@
+
+#This is my first string here:
+
+print("Something here")
+
+#This is the first line to print. This prints the "+" and then 4 times the "-" and then again
+print("+", "-"*4,"+","-" * 4,"+")
+
+#This prints the inside rows. This prints the | and then adds empy spaces
+print("|",  " " * 4, "|", " " * 4 , "|")
+print("|",  " " * 4, "|", " " * 4 , "|")
+print("|",  " " * 4, "|", " " * 4 , "|")
+print("|",  " " * 4, "|", " " * 4 , "|")
+
+#This repeats the middle row
+print("+", "-"*4,"+","-" * 4,"+")
+
+#This prints the last 4 rows
+print("|",  " " * 4, "|", " " * 4 , "|")
+print("|",  " " * 4, "|", " " * 4 , "|")
+print("|",  " " * 4, "|", " " * 4 , "|")
+print("|",  " " * 4, "|", " " * 4 , "|")
+
+#This prints the final string
+print("+", "-"*4,"+","-" * 4,"+")
