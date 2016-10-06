9fddb0e6fa6e1db617afaa82dbab907fbfe10027
spencermcghin <spencer.mcghin@rittmanmead.com>
2016-10-06 09:27:27 -0700

    started sum series function

--- students/spencer_mcghin/session2/series.py
+++ students/spencer_mcghin/session2/series.py
@@ -1,13 +1,26 @@
 def fibonacci(n):
-    """Establish bases and then find value at n"""
+    """Establish non 0 bases and then find value at n"""
     if n == 1 or n == 2:
         return 1
     else:
         return fibonacci(n-2)+fibonacci(n-1)
 
-print(fibonacci(5))
+# Test to print 5th value of Fib sequence #
+
+#print(fibonacci(5))
 
 
-#def lucas(n):
+def lucas(n):
+    """Establish non 0 bases and then find value at n"""
+    if n == 1:
+        return 2
+    elif n == 2:
+        return 1
+    else:
+        return lucas(n-1)+lucas(n-2)
+
+def sum_series():
+
+
 
 
