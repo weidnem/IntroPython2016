de073b3b7f4189cb773a97761c8d01ddce82b4db
spencermcghin <spencer.mcghin@rittmanmead.com>
2016-10-06 09:34:16 -0700

    Fizz buzz solution

--- students/spencer_mcghin/session2/fizz_buzz.py
+++ students/spencer_mcghin/session2/fizz_buzz.py
@@ -1,3 +1,12 @@
 def fizz_buzz(n):
     for i in range(n):
-        if n % 3 == 0
+        if i % 3 == 0 and n % 5 == 0:
+            print("FizzBuzz")
+        elif i % 3 == 0:
+            print("Fizz")
+        elif i % 5 == 0:
+            print("Buzz")
+        else:
+            print(i)
+
+print(fizz_buzz(101))
