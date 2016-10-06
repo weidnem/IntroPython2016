a60c64ecc9475108a01543083a96e9ef78985bbe
spencermcghin <spencer.mcghin@rittmanmead.com>
2016-10-06 09:03:16 -0700

    Changed to include 1 as first base

--- students/spencer_mcghin/session2/series.py
+++ students/spencer_mcghin/session2/series.py
@@ -1,14 +1,13 @@
 def fibonacci(n):
     """Establish bases and then find value at n"""
-    if n == 0:
-        return 0
-    elif n == 1:
+    if n == 1 or n == 2:
         return 1
     else:
         return fibonacci(n-2)+fibonacci(n-1)
 
-print(fibonacci(3))
+print(fibonacci(5))
 
 
+#def lucas(n):
 
 
