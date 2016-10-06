81a8e1ddb4182686a6ff00ba82f6c3a3d87f7e4d
Brad Baumel <Baumelbi@gmail.com>
2016-10-06 16:00:48 -0700

    the final version of the grid printer

--- students/baumel/session_02/grid_printer.py
+++ students/baumel/session_02/grid_printer.py
@@ -1,3 +1,6 @@
+"""this is a lab to create grid patterns"""
+
+#first version simple print statement
 def grid_printer():
     print('+ - - - - + - - - - +')
     print('|         |         |')
@@ -11,5 +14,72 @@ def grid_printer():
     print('|         |         |')
     print('+ - - - - + - - - - +')
 
+#version 2 using values as print objects
+def print_grid(n):
+    sides = "|"
+    apex = "+ "
+    topBottom = "- "
+    top = apex+topBottom*n+apex+topBottom*n+apex
+    wall = sides+"  "*(n)+" "
+    count = n
+    print(top)
+    wallMaker(n)
+    print(top)
+    wallMaker(n)
+    print(top)
 
-grid_printer()
+#top helper method
+def topMaker(n,squares):
+    apex = "+ "
+    lines = "- "
+    top = ((apex+lines*n)*squares)+apex
+    print(top)
+
+#wall making helper method
+def wallMaker(n,squares):
+    count = n
+    sides = "|"
+    wall = sides+"  "*(n)+" "
+    #loops through to make enough walls
+    while count !=0:
+        print(wall*(squares+1))
+        count -= 1
+
+#main grid making method
+def print_grid2(squares,size):
+    counter = squares
+    #loop to print the boxes
+    while counter != 0:
+        topMaker(size,squares)
+        wallMaker(size,squares)
+        counter -=1
+    topMaker(size, squares)
+
+
+def main():
+    squares = eval(input("How many squares would you like? "))
+    size = eval(input("what size squares would you like? "))
+    print_grid2(squares, size)
+
+main()
+
+#print_grid2(3,4)
+#print()
+#print_grid2(5,3)
+#print()
+#print_grid2(2,6)
+#print()
+#print_grid2(9,1)
+
+
+
+
+
+
+
+
+
+
+
+
+    
