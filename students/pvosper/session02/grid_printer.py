ea4ac8e0c72c34017b7bc5aa2c97f1165823ffcf
pvosper <pvosper@mac.com>
2016-10-06 23:52:16 -0700

    working version

--- students/pvosper/session02/grid_printer.py
+++ students/pvosper/session02/grid_printer.py
@@ -1,1 +1,28 @@
-print("something")
+# cell: size of each cell
+# grid: # of cells to print
+# aspect: affects ratio of height to width (2 = square-ish)
+
+bar = "|"
+plus = "+"
+minus = "-"
+space = " "
+
+def print_grid(cell = 3, grid = 2, aspect = 2):
+    for i in range(grid):
+        # horizontal
+        for i in range(grid):
+            print(plus+minus*(cell), end = "-")
+        # close horizontal (only 1 is needed)
+        print(plus)
+        # verticals
+        for i in range(cell-aspect):
+            for i in range(grid):
+                print(bar+space*(cell), end = " ")
+            # close every vertical
+            print(bar)
+    # closing horizontal
+    for i in range(grid):
+        print(plus+minus*(cell), end = "-")
+    print(plus)
+
+print_grid(5,8,2)
