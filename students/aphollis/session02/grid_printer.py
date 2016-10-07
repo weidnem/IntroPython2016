9b8cd298ca80c20191438f2831d1aaff13240e35
Adam Hollis <nerfdorito@gmail.com>
2016-10-06 20:54:15 -0700

    my "fancy" grid printer. no error checking for non-int input yet.  more interested in making the grid_builder more efficient.

--- students/aphollis/session02/grid_printer.py
+++ students/aphollis/session02/grid_printer.py
@@ -5,24 +5,31 @@ ROWS = input("Rows: ")
 COLUMNS = input("Columns: ")
 SIZE = input("Size: ")
 
-
 def print_grid(rows, columns, size):
     corner = "+"
-    h_sep = " - "
+    h_part = "-"
     space = " "
-    v_sep = "|"
+    v_part = "|"
 
-    h_separator = h_sep * int(size)
-    make_v_sep = v_sep * int(size)
+    h_separator = ((space + h_part) * int(size)) + space
+    v_separator = v_part + (space * ( (int(size) * 2 ) + 1))
 
+    height = int(size)
     count1 = int(COLUMNS)
     count2 = int(ROWS)
 
-    for n in range(count1):
-        print(corner, end="")
-        print(h_separator, end = "")
-        print(corner, end = "")
+    for n in range(count2):
+        for i in range(count1):
+            print(corner + h_separator, end="")
+        print(corner)
 
+        for n in range(height):
+            for i in range(count1):
+                print(v_separator, end="")
+            print(v_part)
+    for n in range(count1):
+        print(corner + h_separator, end="")
+    print(corner)
 
 print_grid(ROWS, COLUMNS, SIZE)
 
