418dbb5482166c17622a0ebb2550dfdb182f56e4
Madhumita Acharya <madhu84.bi@gmail.com>
2016-10-06 21:02:34 -0700

    session02 - gridprinter

---
+++ students/madhu/session02/GridPrinterExercise.py
@@ -0,0 +1,14 @@
+plus = '+'
+minus = '- '
+
+print plus + ' ' + (minus*4)  + plus + ' ' + (minus*4) + plus
+print ("|         |         |")
+print ("|         |         |")
+print ("|         |         |")
+print ("|         |         |")
+print plus + ' ' + (minus*4)  + plus + ' ' + (minus*4) + plus
+print ("|         |         |")
+print ("|         |         |")
+print ("|         |         |")
+print ("|         |         |")
+print plus + ' ' + (minus*4)  + plus + ' ' + (minus*4) + plus
