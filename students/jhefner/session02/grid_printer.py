5c90898b8d94b9e24a1f7247cafcf06929d36dab
Jack Hefner <jmhthethird@gmail.com>
2016-10-07 11:43:08 -0700

    jhefner: Grid_Printer Part2

--- students/jhefner/session02/grid_printer.py
+++ students/jhefner/session02/grid_printer.py
@@ -18,5 +18,17 @@ def slightly_less_of_a_cheat_grid():
     print("|"+" "*9+"|"+" "*9+"|")
     print("+"+" -"*4+" +"+" -"*4+" +")
 
+def configureable_grid(size):
+    height_width = size-1
+    spaces = (size*2)-1
+    print("+"+" -"*height_width+" +"+" -"*height_width+" +")
+    for i in range(height_width):
+        print("|"+" "*spaces+"|"+" "*spaces+"|")
+    print("+"+" -"*height_width+" +"+" -"*height_width+" +")
+    for i in range(height_width):
+        print("|"+" "*spaces+"|"+" "*spaces+"|")
+    print("+"+" -"*height_width+" +"+" -"*height_width+" +")
+
 #cheat_grid()
-slightly_less_of_a_cheat_grid()
+#slightly_less_of_a_cheat_grid()
+configureable_grid(10)
