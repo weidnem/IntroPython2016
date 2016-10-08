fc28cc44c98c265c9015a07245cf08ff5a40b39a
pvosper <pvosper@mac.com>
2016-10-08 08:43:26 -0700

    this is better

--- students/pvosper/session02/fizzbuzz.py
+++ students/pvosper/session02/fizzbuzz.py
@@ -9,31 +9,54 @@
 
 fb_list = []
 
-def do_list(fizz, buzz, limit):
+fizz = (3, "Fizz")
+buzz = (5, "Buzz")
+fizzbuzz = (fizz[0] * buzz[0], fizz[0] + buzz[0])
+all_izzes = (fizz, buzz, fizzbuzz)
+limit = 100
+
+def do_list():
     for i in range(0, limit + 1):
         fb_list.append(i)
 
-# You could simplify this into loop 3 times
-def do_fizz(fizz, buzz, limit):
-    for i in range(0, limit + 1, fizz):
-        fb_list[i] = "Fizz"
-
-def do_buzz(fizz, buzz, limit):
-    for i in range(0, limit + 1, buzz):
-        fb_list[i] = "Buzz"
-
-def do_fizzbuzz(fizz, buzz, limit):
-    for i in range(0, limit + 1, fizz * buzz):
-        fb_list[i] = "FizzBuzz"
-
-def fizz_buzz(fizz = 3, buzz = 5, limit = 100):
-    do_list(fizz, buzz, limit)
-    do_fizz(fizz, buzz, limit)
-    do_buzz(fizz, buzz, limit)
-    do_fizzbuzz(fizz, buzz, limit)
+def do_fizzbuzz():
+    for i in range(3):
+        for j in range(0, limit + 1, all_izzes[i][0]):
+            fb_list[j] = all_izzes[i][1]
+            
+def fizz_buzz():
+    do_list()
+    do_fizzbuzz()
     print(fb_list[1:limit + 1])
 
-fizz_buzz(3, 5, 100)
+fizz_buzz()
+
+# ===== This works =====
+# def do_list(fizz, buzz, limit):
+#     for i in range(0, limit + 1):
+#         fb_list.append(i)
+# 
+# # You could simplify this into loop 3 times
+# def do_fizz(fizz, buzz, limit):
+#     for i in range(0, limit + 1, fizz):
+#         fb_list[i] = "Fizz"
+# 
+# def do_buzz(fizz, buzz, limit):
+#     for i in range(0, limit + 1, buzz):
+#         fb_list[i] = "Buzz"
+# 
+# def do_fizzbuzz(fizz, buzz, limit):
+#     for i in range(0, limit + 1, fizz * buzz):
+#         fb_list[i] = "FizzBuzz"
+# 
+# def fizz_buzz(fizz = 3, buzz = 5, limit = 100):
+#     do_list(fizz, buzz, limit)
+#     do_fizz(fizz, buzz, limit)
+#     do_buzz(fizz, buzz, limit)
+#     do_fizzbuzz(fizz, buzz, limit)
+#     print(fb_list[1:limit + 1])
+# 
+# fizz_buzz(3, 5, 100)
 
 # ===== This also works =====
 # for i in range(1,101):
