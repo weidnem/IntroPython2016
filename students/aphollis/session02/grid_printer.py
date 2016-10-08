e70dc68412917b0b21bc8b422d8f67a55b7bdddb
Adam Hollis <nerfdorito@gmail.com>
2016-10-07 23:20:18 -0700

    streamling grid_printer.py

--- students/aphollis/session02/grid_printer.py
+++ students/aphollis/session02/grid_printer.py
@@ -1,4 +1,5 @@
 """Simple text grid drawing script
+10/7/16 edit. Moved input typing to function call and eliminated redundant variables to shorten code and improve readability. 
 """
 
 ROWS = input("Rows: ")
@@ -6,30 +7,27 @@ COLUMNS = input("Columns: ")
 SIZE = input("Size: ")
 
 def print_grid(rows, columns, size):
+
     corner = "+"
     h_part = "-"
     space = " "
     v_part = "|"
 
-    h_separator = ((space + h_part) * int(size)) + space
-    v_separator = v_part + (space * ( (int(size) * 2 ) + 1))
+    h_separator = ((space + h_part) * size) + space
+    v_separator = v_part + (space * ((size * 2 ) + 1))
 
-    height = int(size)
-    count1 = int(COLUMNS)
-    count2 = int(ROWS)
-
-    for n in range(count2):
-        for i in range(count1):
+    for n in range(rows):
+        for i in range(columns):
             print(corner + h_separator, end="")
         print(corner)
 
-        for n in range(height):
-            for i in range(count1):
+        for n in range(size):
+            for i in range(columns):
                 print(v_separator, end="")
             print(v_part)
-    for n in range(count1):
+    for n in range(columns):
         print(corner + h_separator, end="")
     print(corner)
 
-print_grid(ROWS, COLUMNS, SIZE)
+print_grid(int(ROWS), int(COLUMNS), int(SIZE))
 
