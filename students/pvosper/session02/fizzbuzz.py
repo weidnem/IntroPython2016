483c0850e5d8da442e778d2698321efed6bb5575
pvosper <pvosper@mac.com>
2016-10-08 08:11:32 -0700

    second pass with functions, but there might be a better way

--- students/pvosper/session02/fizzbuzz.py
+++ students/pvosper/session02/fizzbuzz.py
@@ -7,12 +7,41 @@
     For numbers which are multiples of both three and five print “FizzBuzz” instead.
 '''
 
-for i in range(1,101):
-    if i % 3 == 0 and i % 5 == 0:
-        print("fizzbuzz")
-    elif i % 3 == 0:
-        print("fizz")
-    elif i % 5 == 0:
-        print("buzz")
-    else:
-        print(i)
+fb_list = []
+
+def do_list(fizz, buzz, limit):
+    for i in range(0, limit + 1):
+        fb_list.append(i)
+
+# You could simplify this into loop 3 times
+def do_fizz(fizz, buzz, limit):
+    for i in range(0, limit + 1, fizz):
+        fb_list[i] = "Fizz"
+
+def do_buzz(fizz, buzz, limit):
+    for i in range(0, limit + 1, buzz):
+        fb_list[i] = "Buzz"
+
+def do_fizzbuzz(fizz, buzz, limit):
+    for i in range(0, limit + 1, fizz * buzz):
+        fb_list[i] = "FizzBuzz"
+
+def fizz_buzz(fizz = 3, buzz = 5, limit = 100):
+    do_list(fizz, buzz, limit)
+    do_fizz(fizz, buzz, limit)
+    do_buzz(fizz, buzz, limit)
+    do_fizzbuzz(fizz, buzz, limit)
+    print(fb_list[1:limit + 1])
+
+fizz_buzz(3, 5, 100)
+
+# ===== This also works =====
+# for i in range(1,101):
+#     if i % 3 == 0 and i % 5 == 0:
+#         print("fizzbuzz")
+#     elif i % 3 == 0:
+#         print("fizz")
+#     elif i % 5 == 0:
+#         print("buzz")
+#     else:
+#         print(i)
