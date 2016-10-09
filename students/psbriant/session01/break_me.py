db267bf1cf2b5cdae3fea6cebc97759473847670
pbriant <psbriant@q.com>
2016-10-08 21:29:05 -0700

    Replaced program and function comments with docstrings.

--- students/psbriant/session01/break_me.py
+++ students/psbriant/session01/break_me.py
@@ -1,20 +1,26 @@
-#Name: Paul Briant
-#Date: 10/04/16
-#Class: Introduction to Python
-#Assignment: Session One
-#
-#Description:
-#This program contains four functions, each which results in one of
-#four common exceptions including the NameError, TypeError, SyntaxError, and
-#AttributeError.
+"""
+Name: Paul Briant
+Date: 10/04/16
+Class: Introduction to Python
+Assignment: Session One
+
+Description:
+This program contains four functions, each which results in one of
+four common exceptions including the NameError, TypeError, SyntaxError, and
+AttributeError.
+"""
 
 #-------------------------------------Imports-----------------------------------
 import math
 #------------------------------------Functions----------------------------------
 
-#This function results in a NameError. Zero arguments are accepted and zero
-#values are returned. A None statement is returned at the end of the function.
 def name_error():
+    """
+    This function results in a NameError. Zero arguments are accepted and zero
+    values are returned. A None statement is returned at the end of the
+    function.
+    """
+
     name = input("Please enter your name: ")
     #your_name should be name
     print("Hello " + your_name +
@@ -22,10 +28,13 @@ def name_error():
 
     return None
 
-#This function results in a TypeError. An int is the sole argument accepted and
-#zero values are returned. A None statement is returned at the end of the
-#function.
 def type_error(number):
+    """
+    This function results in a TypeError. An int is the sole argument accepted
+    and zero values are returned. A None statement is returned at the end of the
+    function.
+    """
+
     while number > 2:
         #The operand '/' is not supported for the string '2'.
         number /= '2'
@@ -33,9 +42,12 @@ def type_error(number):
 
     return None
 
-#This function results in a SyntaxError. Zero arguments are accepted and zero
-#values are returned. A None statement is returned at the end of the function.
 def syntax_error():
+    """
+    This function results in a SyntaxError. Zero arguments are accepted and zero
+    values are returned. A None statement is returned at the end of the
+    function.
+    """
     name = input("Please enter your name: ")
     #The print statement requires parentheses in python 3.
     print "Hello " + name +
@@ -43,9 +55,11 @@ def syntax_error():
 
     return None
 
-#This function results in a AttributeError. An int is the sole argument accepted
-#and zero values are returned. A None statement is returned.
 def attribute_error(number):
+    """
+    This function results in a AttributeError. An int is the sole argument
+    accepted and zero values are returned. A None statement is returned.
+    """
     #The function 'expo' does not exist for the math module.
     print(math.expo(number))
 
@@ -53,7 +67,9 @@ def attribute_error(number):
 
 #===============================================================================
 def main():
-
+    """
+    This function calls four functions that return four different errors.
+    """
     name_error()
     type_error(20)
     syntax_error()
