b4f1f3545129c5eda50e4fadfbc0a86babb1feed
spencermcghin <spencer.mcghin@rittmanmead.com>
2016-10-09 12:54:40 -0700

    Finished problem 14. Added another question.

--- students/spencer_mcghin/session2/Homework.py
+++ students/spencer_mcghin/session2/Homework.py
@@ -20,27 +20,30 @@
 # print("Your third variable is:"), third
 # print("Give me a fourth variable: "), fourth
 
-from sys import argv
-
-script, user_name = argv
-prompt = '> '
-
-print("Hi %s, I'm the %s script." % (user_name, script))
-print("I'd like to ask you a few questions.")
-print("Do you like me %s?" % (user_name))
-likes = input(prompt)
-
-print("Where do you live %s?" % (user_name))
-lives = input(prompt)
-
-print("What kind of computer do you have?")
-computer = input(prompt)
-
-print("""
-Alright, so you said %r about liking me.
-You live in %r.  Not sure where that is.
-And you have a %r computer.  Nice.
-""" % (likes, lives, computer))
+# from sys import argv
+#
+# script, user_name = argv
+# prompt = 'Hmmmm? '
+#
+# print("Hi %s, I'm the %s script." % (user_name, script))
+# print("I'd like to ask you a few questions.")
+# print("Do you like me %s?" % (user_name))
+# likes = input(prompt)
+#
+# print("What city do you live in %s?" % (user_name))
+# lives = input(prompt)
+#
+# print("What kind of computer do you have?")
+# computer = input(prompt)
+#
+# print("What part of the city do you live in?")
+# city = input(prompt)
+#
+# print("""
+# Alright, so you said %r about liking me.
+# You live in %r.  Not sure where that is.
+# And you have a %r computer and live in %r.  Nice.
+# """ % (likes, lives, computer, city))
 
 
 
