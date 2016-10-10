f8b0f5b0c4b2112235856ec614c96f9f7db7eb97
pvosper <pvosper@mac.com>
2016-10-09 20:42:47 -0700

    series.py semi-final pass

--- students/pvosper/session02/series.py
+++ students/pvosper/session02/series.py
@@ -1,34 +1,8 @@
-'''
-    3rd LAB exercise from 161004
-
-    Add a function called fibonacci.
-    - The function should have one parameter n.
-    - The function should return the nth value in the fibonacci series.
-    - Ensure that your function has a well-formed docstring
-    
-    Add a function lucas that returns the nth value in the lucas numbers series.
-    - Ensure that your function has a well-formed docstring
-    
-    Add a third function called sum_series with one required parameter and two 
-    optional parameters. The required parameter will determine which element in
-    the series to print. The two optional parameters will have default values
-    of 0 and 1 and will determine the first two values for the series to be 
-    produced.
-
-    Calling this function with no optional parameters will produce numbers from
-    the fibonacci series. Calling it with the optional arguments 2 and 1 will
-    produce values from the lucas numbers. Other values for the optional
-    parameters will produce other series.
-    - Ensure that your function has a well-formed docstring
-    
-    Add a block of code to the end of your series.py module. Use the block to 
-    write a series of assert statements that demonstrate that your three 
-    functions work properly.
-
-    Use comments in this block to inform the observer what your tests do.
-'''
+'''3rd LAB exercise from 161004'''
 
 def fibonacci(n = 25):
+    '''Returns the nth value in the Fibonacci series'''
+    test_length(n)
     fib_list = [0, 1]
     for i in range(3,n+1):
         fib_list.append(fib_list[-1] + fib_list[-2])
@@ -36,6 +10,8 @@ def fibonacci(n = 25):
     return(fib_list[-1])
 
 def lucas(n = 25):
+    '''Returns the nth value in the Lucas series'''
+    test_length(n)
     luc_list = [2, 1]
     for i in range(3,n+1):
         luc_list.append(luc_list[-1] + luc_list[-2])
@@ -43,9 +19,32 @@ def lucas(n = 25):
     return(luc_list[-1])
 
 def sum_series(n = 25, sum0 = 0, sum1 = 1):
+    '''Given two seeds returns the nth value in an integer series that adds 
+    two consecutive terms to obtain the next one'''
+    test_length(n)
+    test_seeds(sum0, sum1)
     sum_list = [sum0, sum1]
     for i in range(3,n+1):
         sum_list.append(sum_list[-1] + sum_list[-2])
-        
     print("series: ", sum_list)
     return(sum_list[-1])
+    
+def test_length(n):
+    # Test for valid series length
+    assert n > 2, "Series must have three or more elements"
+
+def test_seeds(sum0, sum1):
+    # Test for valid series seeds
+    assert sum0 + sum1 > 0, "At least one of the initial values must be > 0"
+
+# ===== Examples =====
+# fibonacci(9)
+# fibonacci(27)
+# fibonacci(2)
+# lucas(7)
+# lucas(11)
+# lucas(0)
+# sum_series(5)
+# sum_series(3,2,1)
+# sum_series(15,1,1)
+# sum_series(9,0,0)
