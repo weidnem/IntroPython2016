4a61b3028b6a453742940c726c04ad2e32a82db7
pbriant <psbriant@q.com>
2016-10-09 22:13:21 -0700

    Wrote FizzBuzz program in main.

--- students/psbriant/session02/fizz_buzz.py
+++ students/psbriant/session02/fizz_buzz.py
@@ -19,9 +19,17 @@ for all numbers devisible by both three and five.
 
 def main():
     """
-
-    """
     
+    """
+    for i in range(1, 101):
+        if i % 5 == 0 and i % 3 == 0:
+            print('FizzBuzz')
+        elif i % 3 == 0:
+            print('Fizz')
+        elif i % 5 == 0:
+            print('Buzz')
+        else:
+            print(i)
 
 if __name__ == '__main__':
     main()
