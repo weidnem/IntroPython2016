c5ea650869eb633eff9444b0093d79346958ff6d
pbriant <psbriant@q.com>
2016-10-10 15:30:39 -0700

    Created fibonacci method.

--- students/psbriant/session02/series.py
+++ students/psbriant/session02/series.py
@@ -11,6 +11,19 @@ Description:
 
 # -------------------------------Functions--------------------------------------
 
+
+def fibonacci(n):
+    """
+    Return the nth value in the Fibonacci series based on integer argument n.
+    """
+    if n == 0:
+        return 0
+    if n == 1:
+        return 1
+    return fibonacci(n-1) + fibonacci(n-2)
+
+
+
 # ==============================================================================
 
 
@@ -18,6 +31,10 @@ def main():
     """
 
     """
-    
+    print(fibonacci(3))
+    print(fibonacci(2))
+    print(fibonacci(0))
+    print(fibonacci(7))
+
 if __name__ == '__main__':
     main()
