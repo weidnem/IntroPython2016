4645b298fe415c9dc622cdf5f7d13f7f29f0a148
pbriant <psbriant@q.com>
2016-10-09 22:06:08 -0700

    Created fizz_buzz.py, set up program and added docstring.

---
+++ students/psbriant/session02/fizz_buzz.py
@@ -0,0 +1,27 @@
+"""
+Name: Paul Briant
+Date: 10/11/16
+Class: Introduction to Python
+LAB: FIZZBUZZ
+
+Description:
+This program iterates through a range of numbers and prints each number. The
+string 'Fizz' is printed for all numbers devisible by three. The string 'Buzz'
+is printed for all numbers devisible by five. The string 'FizzBuzz' is printed
+for all numbers devisible by both three and five.
+"""
+# -------------------------------Import-----------------------------------------
+
+# -------------------------------Functions--------------------------------------
+
+# ==============================================================================
+
+
+def main():
+    """
+
+    """
+    
+
+if __name__ == '__main__':
+    main()
