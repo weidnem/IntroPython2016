6accabda87d7322ca89ba0e3c7a2b70742f386ef
pbriant <psbriant@q.com>
2016-10-10 22:03:15 -0700

    Answer to exercise 12.

--- students/psbriant/session02/learn_pythonex.py
+++ students/psbriant/session02/learn_pythonex.py
@@ -30,7 +30,11 @@ def exercise_12():
     """
     Answer for exercise 12.
     """
-    
+    age = input("How old are you? ")
+    height = input("How tall are you? ")
+    weight = input("How much do you weigh? ")
+
+    print("So, you're %r old, %r tall and %r heavy.") % (age, height, weight)
 
 
 # ==============================================================================
