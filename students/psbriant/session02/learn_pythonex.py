6dad007692196805e208c61867dc6834ed9294eb
pbriant <psbriant@q.com>
2016-10-10 21:07:43 -0700

    Created learn_pythonex.py module and established organizational structure.

---
+++ students/psbriant/session02/learn_pythonex.py
@@ -0,0 +1,25 @@
+"""
+Name: Paul Briant
+Date: 10/11/16
+Class: Introduction to Python
+Exercises: Learn Python the Hard Way problems 11-14, 18, 19, 21, 28-33
+
+Description:
+Answers to exercises from Learn Python the Hard Way.
+"""
+# -------------------------------Import-----------------------------------------
+
+# -------------------------------Functions--------------------------------------
+
+
+# ==============================================================================
+
+
+def main():
+    """
+    Tests each method to verify specified parameters return expected output.
+    """
+
+
+if __name__ == '__main__':
+    main()
