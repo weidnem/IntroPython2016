7b47339fcb3566cff690695ca6082973f6b46fc7
Madhumita Acharya <madhu84.bi@gmail.com>
2016-10-10 19:50:10 -0700

    Session-02 Exercises

--- students/madhu/session02/series.py
+++ students/madhu/session02/series.py
@@ -6,7 +6,7 @@ def fibonacci(n): #function to print out the nth element in the fibonacci series
         value = lst[i-1] + lst[i-2]
         lst.insert(i,value)
     
-    print lst[n]
+    print ("The nth fibonacci number is: " + str(lst[n]))
 
 
 def lucas(n): #function to print out the nth element in the lucas numbers series#
@@ -17,12 +17,27 @@ def lucas(n): #function to print out the nth element in the lucas numbers series
         value = lst[i-1] + lst[i-2]
         lst.insert(i,value)
 
-    print lst[n]
+    print ("The nth lucas number is: " + str(lst[n]))
 
+def sum_series(n,first=0,second=1): #generalized function to return the nth in the series that starts with numbers specified in the first and second optional parameters# 
+    lst = []
+    lst.insert(0,first)
+    lst.insert(1,second)
+    for i in range(2,n+1):
+        value = lst[i-1] + lst[i-2]
+        lst.insert(i,value)
+
+    print ("The nth number in the series is: " + str(lst[n]))
 
 
 
 if __name__ == "__main__":
     fibonacci(10) #calling the fibonacci function to return the 10th element in the series#
+
     lucas(10) #calling the lucas function to retrun the 10th element in the series#
 
+    sum_series(10) #calling the generalized function, sum_series without the optional parameters to generate the fibonacci number#
+
+    sum_series(10,2,1) #calling the generalized function, sum_series to generate the lucas number#
+
+    sum_series(10,3,2) #calling the generalized function, sum_series to generate the nth number in a random series#
