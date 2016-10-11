da2eda915f2f0baf40e8b494de72ab4158da63af
pbriant <psbriant@q.com>
2016-10-11 11:52:49 -0700

    Deleted the original grid_line method and changed name of grid_print to grid_line.

--- students/psbriant/session02/grid_printer.py
+++ students/psbriant/session02/grid_printer.py
@@ -11,22 +11,16 @@ This program builds a grid based on specific size parameters.
 
 # -------------------------------Functions--------------------------------------
 
-def grid_print():
+
+def grid_line():
+    """
+    Print each row of grid.
+    """
     print('+' + (2 * ('-' * 4 + '+')))
     print((('|' + (2 * (' ' * 4 + '|')) + '\n') * 4), end='')
     print('+' + (2 * ('-' * 4 + '+')))
     print((('|' + (2 * (' ' * 4 + '|')) + '\n') * 4), end='')
     print('+' + (2 * ('-' * 4 + '+')))
-
-def grid_line(char1, char2, char2num, number):
-    """
-    This function takes in two single character strings and two integers and
-    prints each row of the grid as specified.
-    """
-    for i in range(number):
-        print_grid(char1, char2, char2num)
-
-    return None
 
 
 def print_grid(char1, char2, char2num):
@@ -35,9 +29,9 @@ def print_grid(char1, char2, char2num):
     prints each row of the grid as specified.
     """
 
-
-
 # ==============================================================================
+
+
 def main():
     """
     Builds a grid based on a specific character and size parameters.
