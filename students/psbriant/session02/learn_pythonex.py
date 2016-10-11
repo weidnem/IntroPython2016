19f8f201fa88fd05db348fcf8dc23b26569db898
pbriant <psbriant@q.com>
2016-10-10 21:57:45 -0700

    Answer to exercise 11.

--- students/psbriant/session02/learn_pythonex.py
+++ students/psbriant/session02/learn_pythonex.py
@@ -12,14 +12,36 @@ Answers to exercises from Learn Python the Hard Way.
 # -------------------------------Functions--------------------------------------
 
 
+def exercise_11():
+    """
+    Answer for exercise 11.
+    """
+    print("How old are you?"),
+    age = input()
+    print("How tall are you?"),
+    height = input()
+    print("How much do you weigh?"),
+    weight = input()
+
+    print("So, you're %r old, %r tall and %r heavy." % (age, height, weight))
+
+
+def exercise_12():
+    """
+    Answer for exercise 12.
+    """
+    
+
+
 # ==============================================================================
 
 
 def main():
     """
-    Tests each method to verify specified parameters return expected output.
+    Test each method to verify specified parameters return expected output.
     """
-
+    exercise_11()
+    exercise_12()
 
 if __name__ == '__main__':
     main()
