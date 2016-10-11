0f971e67f477a06f6b82b17dc1d72182ab121ae3
Madhumita Acharya <madhu84.bi@gmail.com>
2016-10-10 23:36:10 -0700

    Session-02 Exercises

--- students/madhu/session02/GridPrinterExercise.py
+++ students/madhu/session02/GridPrinterExercise.py
@@ -2,13 +2,39 @@ def print_grid(n):
 
     plus = '+'
     minus = '- '
-    total_minus_spaces = n - 2
+
+    cell_size = n // 2
+    length = cell_size + (cell_size - 1) + 2
+    ends = '+ ' + ('- ' * cell_size)
+    bars = '|' + (' ' * length)
     
-    for i in range(0,n-1):
-        print (plus + ' ' + (minus*size)  + plus + ' ' + (minus*size) + plus)
-        for i in range(0,size):
-            print ("|" + (spaces * ' ') + "|" + (spaces * ' ') + "|" + (spaces * ' '))
-    
+    for i in range(0,2):
+        print ((ends * 2) + '+')
+        for i in range(0,cell_size):
+            print ((bars * 2) + '|')
+    print ((ends * 2) + '+')
 
 
-print_grid(5)
+
+print_grid(10)
+print_grid(15)
+
+
+def print_grid2(rows,cell_size):
+
+    plus = '+'
+    minus = '- '
+    length = cell_size + (cell_size - 1) + 2
+    bars = '|' + (' ' * length)
+    ends = '+ ' + ('- ' * cell_size)
+
+    for i in range(0,rows):
+        print ((ends * rows) + '+')
+        for i in range(0,cell_size):
+            print ((bars * rows) + '|')
+    print ((ends * rows) + '+')
+
+
+print_grid2(3,4)
+print_grid2(2,3)
+print_grid2(5,3)
