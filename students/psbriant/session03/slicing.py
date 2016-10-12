5133309237b9d1539cd59d78df40b6e7e4f6a69c
pbriant <psbriant@q.com>
2016-10-11 19:34:31 -0700

    Created slicing.py module and added five sequence manipulating methods.

---
+++ students/psbriant/session03/slicing.py
@@ -0,0 +1,61 @@
+"""
+Name: Paul Briant
+Date: 10/11/16
+Class: Introduction to Python
+LAB: Slicing
+
+Description:
+
+"""
+# -------------------------------Import-----------------------------------------
+
+# -------------------------------Functions--------------------------------------
+
+
+def first_last(s):
+    """
+
+    """
+    return s[-1] + s[1:-1] + s[0]
+
+
+def every_other(s):
+    """
+
+    """
+    return s[::2]
+
+
+def four_removed(s):
+    """
+
+    """
+    return s[3:-3:2]
+
+
+def reversed(s):
+    """
+
+    """
+    return s[::-1]
+
+
+def third_is_the_word(s):
+    """
+
+    """
+    t = len(s) // 3
+    return s[t:t * 2] + s[t * 2 :] + s[:t]
+
+
+# ==============================================================================
+
+
+def main():
+    """
+    Hi
+    """
+    every_other('mississippi')
+
+if __name__ == '__main__':
+    main()
