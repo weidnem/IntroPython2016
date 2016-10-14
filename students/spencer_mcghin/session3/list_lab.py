f1d7de0d6b91f1bc6085524c84eb8b9117607b82
spencermcghin <spencer.mcghin@rittmanmead.com>
2016-10-13 20:08:50 -0700

    Made some changes to the list lab

--- students/spencer_mcghin/session3/list_lab.py
+++ students/spencer_mcghin/session3/list_lab.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python3
 
 """Generate and display list of fruits"""
+print("Here's a list of fruits:")
 fruits = ["Apples", "Pears", "Oranges", "Peaches"]
 for fruit in fruits:
     print(fruit)
 
 """Prompt user for another fruit and add to list"""
-new_fruit = input("Please tell me which fruit I should add to the list > ")
+new_fruit = input("Please enter a new fruit I should add to the list > ")
 fruits.append(new_fruit)
 print("Your new list of fruits is:")
 for fruit in fruits:
@@ -14,10 +15,10 @@ for fruit in fruits:
 
 """Prompt user for a number that corresponds to a fruit and return that number and fruit"""
 new_number = int(input("Please give me a number from 1 to 5 > "))
-while new_number > 5:
+while new_number > 5 or new_number <= 0:
     new_number = int(input("Quit being difficult and give me a number between 1 and 5 > "))
 else:
-    print("Item: " + str(new_number) + " " + "is" + " " + fruits[new_number - 1])
+    print("Item " + str(new_number) + " " + "is" + " " + fruits[new_number - 1])
 
 """Add another fruit to the beginning of the list"""
 first_fruit = [input("Now give me another fruit to add to the beginning of the list > ")]
@@ -29,6 +30,7 @@ for fruit in new_fruits_list:
 """Add another fruit to the beginning of the list using the .insert method"""
 second_fruit = input("For the last time, give me a fruit to add to the list > ")
 new_fruits_list.insert(0, second_fruit)
+print("Your final list of fruits is...drumroll please...:")
 for fruit in new_fruits_list:
     print(fruit)
 
