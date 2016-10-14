bddf8309165fb29d27a0662918f14a8f2bd016e3
pbriant <psbriant@q.com>
2016-10-14 12:48:42 -0700

    Updates to series1 method.

--- students/psbriant/session03/list_lab.py
+++ students/psbriant/session03/list_lab.py
@@ -12,7 +12,12 @@ Description:
 # -------------------------------Import-----------------------------------------
 
 # -------------------------------Functions--------------------------------------
+
+
 def series1(fruit):
+    """
+
+    """
     # Add some cool text formatting
     print(fruit)
     new_fruit = input("Please enter another fruit ")
@@ -20,7 +25,7 @@ def series1(fruit):
     # Add some cool text formatting
     print(fruit)
     number = input("Please enter a number ")
-    # Add some cool text formatting
+    # Add some cool text formatting, ask about one is first basis.
     print(number, fruit(number))
     fruit = ["Watermelon"] + fruit
     # Add some cool text formatting
@@ -28,18 +33,26 @@ def series1(fruit):
     fruit.insert("Pineapple", 0)
     # Add some cool text formatting
     print(fruit)
+    # displays all fruits beginning with the letter 'P'
+    for item in fruit:
+        if item[0] == 'P':
+            print(item)
 
+    def series2(fruit):
+    """
 
-
-
+    """
+    print(fruit)
+    
 # ==============================================================================
+
 
 def main():
     """
 
     """
-    # fruit = ["Apples", "Pears", "Oranges", "Peaches"]
-     # series1(fruit)
+    fruit = ["Apples", "Pears", "Oranges", "Peaches"]
+    series1(fruit)
 
 
 if __name__ == '__main__':
