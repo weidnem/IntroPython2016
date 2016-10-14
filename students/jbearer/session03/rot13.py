08b7360f9da3152c417a9906259478394d72b2a5
Jerry Bearer <jbearer@gmail.com>
2016-10-13 22:05:39 -0700

    ROT13: built dictionary

---
+++ students/jbearer/session03/rot13.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""
+Excercise --  ROT13
+October 13, 2016
+"""
+
+if __name__ == '__main__':
+
+    d_list = {'a':'n','b':'o','c':'p','d':'q','e':'r','f':'s','g':'t',
+    'h':'u','i':'v','j':'w','k':'x','l':'y','m':'z','n':'a','o':'b','p':'c',
+    'q':'d','r':'e','s':'f','t':'g','u':'h','v':'i','w':'j','x':'k','y':'l',
+    'z':'m'}
+
+    print(d_list)
+
+
+
+    def rot13():
+        print('hi')
