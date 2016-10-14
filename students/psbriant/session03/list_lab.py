e6f472b2555408904746c59661476c71ddda71d0
pbriant <psbriant@q.com>
2016-10-13 21:04:49 -0700

    Created series1 function.

--- students/psbriant/session03/list_lab.py
+++ students/psbriant/session03/list_lab.py
@@ -4,7 +4,7 @@
 Name: Paul Briant
 Date: 10/11/16
 Class: Introduction to Python
-LAB: Slicing
+LAB: List Lab
 
 Description:
 
@@ -12,6 +12,24 @@ Description:
 # -------------------------------Import-----------------------------------------
 
 # -------------------------------Functions--------------------------------------
+def series1(fruit):
+    # Add some cool text formatting
+    print(fruit)
+    new_fruit = input("Please enter another fruit ")
+    fruit.append(new_fruit)
+    # Add some cool text formatting
+    print(fruit)
+    number = input("Please enter a number ")
+    # Add some cool text formatting
+    print(number, fruit(number))
+    fruit = ["Watermelon"] + fruit
+    # Add some cool text formatting
+    print(fruit)
+    fruit.insert("Pineapple", 0)
+    # Add some cool text formatting
+    print(fruit)
+
+
 
 
 # ==============================================================================
@@ -20,7 +38,9 @@ def main():
     """
 
     """
-    fruit = []
+    # fruit = ["Apples", "Pears", "Oranges", "Peaches"]
+     # series1(fruit)
+
 
 if __name__ == '__main__':
     main()
