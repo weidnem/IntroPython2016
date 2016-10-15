237126a19c8c7a4d8a908b653420b71050e6f097
spencermcghin <spencer.mcghin@rittmanmead.com>
2016-10-14 17:27:38 -0700

    Mailroom lab attempt 2

---
+++ students/spencer_mcghin/session3/mailroom_new.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+"""Dictionary of Donors and Amounts Donated"""
+
+donors = {"Nick Padgett": [12312, 34230, 38593],
+          "Julia Allen": [49203, 5023, 9052],
+          "Pete Tamisin": [9503, 2093, 10932, 40923],
+          "Charles Elliott": [209, 50912, 9026],
+          "Andy Rocha": [20968, 2091, 8934],
+          "Beth DeSousa": [29092, 5906, 8734]}
+
+"""
+
+Functions for program
+
+"""
+
+def print_donor_list(
