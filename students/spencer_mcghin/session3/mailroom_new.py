50c1951440cfa7fe27d69ee5baa0efd8bc2f44f4
spencermcghin <spencer.mcghin@rittmanmead.com>
2016-10-14 19:41:42 -0700

    Added functions for mailroom lab

--- students/spencer_mcghin/session3/mailroom_new.py
+++ students/spencer_mcghin/session3/mailroom_new.py
@@ -15,4 +15,36 @@ Functions for program
 
 """
 
-def print_donor_list(
+"""Print donor list"""
+
+def print_donor_list():
+    for donor, donation in donors.items():
+        print(donor)
+
+"""Add donor name to list"""
+
+def add_donor():
+    donors.update(# need donor name variable here #)
+
+"""Add donation amount to new donor"""
+
+def add_amount():
+    donors.update({# add_donor variable : amount_input variable #})
+
+"""Vefify donation amount is an integer"""
+
+def check_donation():
+    while isinstance( # input #, int):
+        add_amount()
+    else:
+
+"""Print email to terminal"""
+
+def print_email():
+        print("Hello %r, Thank you so much for your generous donation to our very charitable organization." '\n'
+              "Your contribution will help us to further our reach in providing charitable services to those in need." '\n'
+              "We hope that you will continue to support our organization in the future." '\n'
+              "Sincerely," '\n'
+              '\n'
+              "Spencer McGhin")
+
