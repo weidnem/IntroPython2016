f168c08117aceb5ab87d1218ed4eeef9b0a2cdf1
pbriant <psbriant@q.com>
2016-10-15 13:15:42 -0700

    Removed unessecary comments.

--- students/psbriant/session03/list_lab.py
+++ students/psbriant/session03/list_lab.py
@@ -18,20 +18,17 @@ def series1(fruit):
     """
 
     """
-    # Add some cool text formatting
     print(fruit)
     new_fruit = input("Please enter another fruit ")
     fruit.append(new_fruit)
     # Add some cool text formatting
     print(fruit)
     number = input("Please enter a number ")
-    # Add some cool text formatting, ask about one is first basis.
+    # Ask about 'one is first basis'.
     print(number, fruit(number))
     fruit = ["Watermelon"] + fruit
-    # Add some cool text formatting
     print(fruit)
     fruit.insert("Pineapple", 0)
-    # Add some cool text formatting
     print(fruit)
     # displays all fruits beginning with the letter 'P'
     for item in fruit:
