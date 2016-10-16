9cfbd5c2ab2f257b738d42f30ec3a8cd90c4dc79
pbriant <psbriant@q.com>
2016-10-16 16:26:43 -0700

    Created donors data structure.

--- students/psbriant/session03/mailroom.py
+++ students/psbriant/session03/mailroom.py
@@ -20,6 +20,11 @@ def main():
     """
 
     """
+    donors = [['Bill Gates', [1000000, 2500000, 1800000]],
+              ['Sheryl Sandberg', [200000, 100000, 130000]],
+              ['Larry Page', [150000, 110000, 170000]],
+              ['Satya Nadella', [140000, 111000]],
+              ['Susan Wojcicki', [100000, 109000]]]
 
 
 if __name__ == '__main__':
