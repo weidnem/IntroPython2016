4562b63f2bc07b84652687513dca8315a9e0abab
komashu <shulatif@gmail.com>
2016-10-15 21:21:12 -0700

    adding my three labs. (#44)

--- students/shulatif/session_02/grid_printer.py
+++ students/shulatif/session_02/grid_printer.py
@@ -3,11 +3,10 @@ grid printer
 """
 
 
-def grid(i):
-    cross = '+ ' + '- ' * i + '+ ' + '- ' * i + '+\n'
+def grid(i, x):
+    cross = '+ ' + '- ' * i + '+ ' + '- ' * i + '+\n' '| ' + '  ' * i + '| ' + '  ' * i + '|\n'
     blanks = '| ' + '  ' * i + '| ' + '  ' * i + '|\n'
     middle = blanks * i
-
     print(cross+middle+cross+middle+cross)
 
 grid(5)
