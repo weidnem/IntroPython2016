c521dd53b1989657d35cb9568306b97ae9b6d626
pbriant <psbriant@q.com>
2016-10-16 12:37:16 -0700

    Created str_format1 method and added function calls in main to test output.

--- students/psbriant/session03/string_lab.py
+++ students/psbriant/session03/string_lab.py
@@ -10,11 +10,24 @@ Description:
 
 """
 # -------------------------------Functions--------------------------------------
+
+
+def str_format1(num_list):
+    """
+
+    """
+    print("File {:03d}: \t{:.2f}, {:.0e}".format(num_list[0], num_list[1],
+          num_list[2]))
+
+
 def main():
     """
 
     """
-
+    num_list = [2, 123.4567, 10000]
+    str_format1(num_list)
+    str_format1([10, 1.999, 100])
+    str_format1([100, 0.117, 567])
 
 if __name__ == '__main__':
     main()
