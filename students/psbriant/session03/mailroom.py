32693dcb3be50131078a49764a43bee655d917f2
pbriant <psbriant@q.com>
2016-10-16 13:44:56 -0700

    Created mailroom.py and added basic organizational structure.

---
+++ students/psbriant/session03/mailroom.py
@@ -0,0 +1,26 @@
+# !/usr/bin/env python3
+
+"""
+Name: Paul Briant
+Date: 10/18/16
+Class: Introduction to Python
+Session: 03
+Assignment: Mailroom
+
+Description:
+
+"""
+# -------------------------------Functions--------------------------------------
+
+
+# ==============================================================================
+
+
+def main():
+    """
+
+    """
+
+
+if __name__ == '__main__':
+    main()
