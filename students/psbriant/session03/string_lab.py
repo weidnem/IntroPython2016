42d33fa792bb85b380b463485362bb7af9b0169f
pbriant <psbriant@q.com>
2016-10-15 17:39:56 -0700

    Created string_lab.py and added organizational structure.

---
+++ students/psbriant/session03/string_lab.py
@@ -0,0 +1,20 @@
+"""
+Name: Paul Briant
+Date: 10/18/16
+Class: Introduction to Python
+Session: 03
+Assignment String Lab
+
+Description:
+
+
+"""
+# -------------------------------Functions--------------------------------------
+def main():
+    """
+
+    """
+
+
+if __name__ == '__main__':
+    main()
