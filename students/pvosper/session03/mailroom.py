a46ce24bbcdc568ea267dc859bf860cd6190a179
pvosper <pvosper@mac.com>
2016-10-15 20:53:13 -0700

    OK, got send mail working

--- students/pvosper/session03/mailroom.py
+++ students/pvosper/session03/mailroom.py
@@ -2,6 +2,8 @@
 
 print("You've Got Mail!")
 
+# ===== Initial data set =====
+
 donor_list = [
     ["Domingo Nair", [75, 50, 25]],
     ["Cruz Woodman", [100]],
@@ -25,6 +27,8 @@ donor_list = [
     ["Kurt Cowherd", [100, 50, 25]]
     ]
 
+# ===== Utilities =====
+
 def donor_total(index):
     donations = 0
     for entry in donor_list[index][1]:
@@ -40,19 +44,12 @@ def find_donor(str):
         check = entry[0].lower()
         if check.find(str.lower()) > -1:
             return donor_list[donor_list.index(entry)]
-    return
+    return False
 
 def second_element(list):
     return list[1]
 
-def report_donors():
-    temp_list = []
-    for i in range(len(donor_list)):
-        entry_donation_total = donor_total(i)
-        temp_list.append([donor_list[i][0], entry_donation_total])
-    temp_list = sorted(temp_list, key=second_element, reverse=True)
-    for donor in temp_list:
-        print(donor[0], "\t$", donor[1]) # $todo: better string formatting
+# ===== Main Elements =====
 
 start_message = ('''
 Choose from one of the following actions:
@@ -61,7 +58,52 @@ Choose from one of the following actions:
     x   Exit the program
 ''')
 
-def mailroom_script():
+def mail_message(donor_info):
+    donations = 0
+    for entry in donor_info[1]:
+        donations += entry
+    first_name = donor_info[0][:donor_info[0].index(' ')]
+    form = '''\
+    Dear {name},
+    Thank you so much for your generous donation of ${amount:0.2f}.
+    Without such selfless contributions from people like you, we
+    would not be able to continue this important work.
+    We are all very appreciative of your support!
+    
+    Sincerely,
+    
+    Carl
+    '''
+    print(form.format(name=first_name, amount=donations))
+
+def mail_thank_you():
+    target_donor = []
+    print('''
+    First you'll need to select a donor to mail.
+    Enter a full or partial name to search for:
+    ''')
+    response = input().strip().lower()
+    target_donor = find_donor(response)
+    if target_donor != False:
+        print("Mail to:", target_donor[0])
+        print("Is this correct? (y/n)")
+        correct_response = input().strip().lower()
+        if correct_response == "y":
+            print(mail_message(target_donor))
+            return
+    else:
+        print("Sorry, but {} could not be found".format(response))
+        return
+    
+def report_donors():
+    temp_list = []
+    for i in range(len(donor_list)):
+        temp_list.append([donor_list[i][0], donor_total(i)])
+    temp_list = sorted(temp_list, key=second_element, reverse=True)
+    for donor in temp_list:
+        print(donor[0], "\t$", donor[1]) # $todo: better string formatting
+
+def main():
 
     while True:
 
@@ -70,13 +112,15 @@ def mailroom_script():
         response = input().strip().lower()
     
         if response == "m":
-            print("list")
+            print("This will allow you to pick a donor, then send a Thank You message")
+            mail_thank_you()
         
         elif response == "r":
             print("Report of all donors, sorted by donation size:")
             report_donors()
 
         elif response == "x":
+            print("Exiting program")
             break
     
         else:
