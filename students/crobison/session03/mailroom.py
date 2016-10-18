eb43e0080ca6abfefab7fc1ffe210fa125a54aaf
Robison, Charles <Charles.Robison@cdk.com>
2016-10-17 21:08:26 -0700

    multiple changes but laptop struggling! status git status

--- students/crobison/session03/mailroom.py
+++ students/crobison/session03/mailroom.py
@@ -2,6 +2,8 @@
 # 2016.10.16
 # Mailroom Lab
 
+# Initial version using dictionary.  Abandoning for now...
+
 #!/usr/bin/env python
 
 donors = {
@@ -18,12 +20,27 @@ for k, v in donors.items():
 
 def print_report():
     print("This will print a report")
+    print("Name","\t","Donated","\t", "Times Donated", "\t", "Avg Donated")
     for i in donations:
-        print(i, donations[i])
+        print(i, "\t", donations[i], "\t", len(donors[i]), "\t", donations[i]/len(donors[i]))
 
 
 def send_thanks():
     print("This will write a thank you note")
+    while True:
+        print(ty_msg)
+        response = input("==> ").strip()
+
+        if response == 'list':
+            for i in donations:
+                print(i)
+        elif response == 'add':
+            pass
+        elif response == 'x':
+            break
+        else:
+            print('please type "list", "add" or "x"')
+
 
 
 # here is where triple quoted strings can be helpful
@@ -31,6 +48,13 @@ msg = """
 What would you like to do?
 To send a thank you: type "s"
 To print a report: type "p"
+To exit: type "x"
+"""
+
+ty_msg = """
+Would you like to see a list of donors or add a new donor?
+To see a list: type "list"
+To enter a new donor: type "add"
 To exit: type "x"
 """
 
