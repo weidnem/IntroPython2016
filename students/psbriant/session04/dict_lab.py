11f07e310260cc239bc1664b86211bc682cdce8f
pbriant <psbriant@q.com>
2016-10-19 16:40:33 -0700

    Created block of code in main that creates a dictionary, deletes keys, adds keys and check to determine whether certain keys and values are present.

--- students/psbriant/session04/dict_lab.py
+++ students/psbriant/session04/dict_lab.py
@@ -1,6 +1,6 @@
 """
 Name: Paul Briant
-Date: 10/18/16
+Date: 10/24/16
 Class: Introduction to Python
 Session04
 LAB: Dictionaries
@@ -21,7 +21,14 @@ def main():
     """
     dict1 = {"name": "Chris", "city": "Seattle", "cake": "Chocolate"}
     print(dict1)
-    
+    del dict1["cake"]
+    print(dict1)
+    dict1["fruit"] = "Mango"
+    print(dict1.keys())
+    print(dict1.values())
+    print("cake" in dict1)
+    print("Mango" in dict1.values())
+
 
 
 if __name__ == '__main__':
