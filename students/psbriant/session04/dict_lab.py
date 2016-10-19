7d4bcbbe542279e67c233f351edfdf2418991c52
pbriant <psbriant@q.com>
2016-10-18 21:11:21 -0700

    Created dict_lab.py.

---
+++ students/psbriant/session04/dict_lab.py
@@ -0,0 +1,28 @@
+"""
+Name: Paul Briant
+Date: 10/18/16
+Class: Introduction to Python
+Session04
+LAB: Dictionaries
+
+Description:
+
+"""
+# -------------------------------Import-----------------------------------------
+
+# -------------------------------Functions--------------------------------------
+
+# ==============================================================================
+
+
+def main():
+    """
+
+    """
+    dict1 = {"name": "Chris", "city": "Seattle", "cake": "Chocolate"}
+    print(dict1)
+    
+
+
+if __name__ == '__main__':
+    main()
