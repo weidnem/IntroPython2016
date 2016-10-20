d68a49bddc010b8a45ab0ce25ba978367bc09e91
pvosper <pvosper@mac.com>
2016-10-20 06:29:10 -0700

    finishing assignment

--- students/pvosper/session04/dict_lab.py
+++ students/pvosper/session04/dict_lab.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python3
 
 '''
+When the script is run, it should accomplish the following four series of actions:
+
+=== ONE ===
 Create a dictionary containing “name”, “city”, and “cake” for “Chris” from “Seattle” who likes “Chocolate”.
 Display the dictionary.
 Delete the entry for “cake”.
@@ -10,20 +13,41 @@ Display the dictionary keys.
 Display the dictionary values.
 Display whether or not “cake” is a key in the dictionary (i.e. False) (now).
 Display whether or not “Mango” is a value in the dictionary (i.e. True).
+
+=== TWO ===
+Using the dictionary from item 1: Make a dictionary using the same keys but with the number of ‘t’s in each value.
+
+=== THREE ===
+Create sets s2, s3 and s4 that contain numbers from zero through twenty, divisible 2, 3 and 4.
+Display the sets.
+Display if s3 is a subset of s2 (False)
+and if s4 is a subset of s2 (True).
+
+=== FOUR ===
+Create a set with the letters in ‘Python’ and add ‘i’ to the set.
+Create a frozenset with the letters in ‘marathon’
+display the union and intersection of the two sets.
+
 '''
+
+# Initial dictionaries and utilities
 
 d = {'name' : 'Chris', 'city' : 'Seattle', 'cake' : 'chocolate'}
 
-# d.keys()
-# dict_keys(['score', 'name'])
-# 
-# d.values()
-# dict_values([42, 'Brian'])
-# 
-# d.items()
-# dict_items([('score', 42), ('name', 'Brian')])
+def count_s(str):
+    tally = 0  
+    for char in str:
+        if char.lower() == 's':
+            tally += 1
+    return tally
 
-if __name__ == "__main__":
+d2 = {}
+for key in d:
+    d2[key] = count_s(d[key])
+
+if __name__ == '__main__':
+    print('\n=== ONE ===')
+
     print('\nDisplay the dictionary.')
     print(d)
     
@@ -49,8 +73,49 @@ if __name__ == "__main__":
     print('\nDisplay whether or not “Mango” is a value in the dictionary (i.e. True).')
     print("'Mango' in d.values() =", "Mango" in d.values())
     
+    print('\n=== TWO ===')
 
+    print('\nUsing the dictionary from item 1: Make a dictionary using the same keys but with the number of ‘t’s in each value.')
 
+    print('\nDisplay the dictionary.')
+    print(d2)
+    
+    print('\n=== THREE ===')
+    
+    print('\nCreate sets s2, s3 and s4 that contain numbers from zero through twenty, divisible 2, 3 and 4.')
+    s2 = set(range(0, 21, 2))
+    s3 = set(range(0, 21, 3))
+    s4 = set(range(0, 21, 4))
+    
+    print('\nDisplay the sets.')
+    print('\ns2: ', s2)
+    print('\ns3: ', s3)
+    print('\ns4: ', s4)
+    
+    print('\nDisplay if s3 is a subset of s2 (False)')
+    print(s3.issubset(s2))
+    
+    print('\nand if s4 is a subset of s2 (True).')
+    print(s4.issubset(s2))
+    
 
     
+    
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     
