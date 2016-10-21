fe638a08905a7fd1b7b7e9988371f556bb137522
Robison, Charles <Charles.Robison@cdk.com>
2016-10-21 12:24:42 -0700

    Initial commit

---
+++ students/crobison/session04/dict_lab.py
@@ -0,0 +1,9 @@
+# Charles Robison
+# 2016.10.18
+# Dictionary and Set Lab
+
+# Create a dictionary containing “name”, “city”, and “cake”
+# for “Chris” from “Seattle” who likes “Chocolate”.
+
+d = {'name': 'Chris', 'city': 'Seattle', 'cake': 'Chocolate'}
+
