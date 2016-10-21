18f29b2b1a99614b09591df4a60c1670c845aa9b
Robison, Charles <Charles.Robison@cdk.com>
2016-10-21 12:45:36 -0700

    Add first set of exercises.

--- students/crobison/session04/dict_lab.py
+++ students/crobison/session04/dict_lab.py
@@ -4,6 +4,55 @@
 
 # Create a dictionary containing “name”, “city”, and “cake”
 # for “Chris” from “Seattle” who likes “Chocolate”.
-
 d = {'name': 'Chris', 'city': 'Seattle', 'cake': 'Chocolate'}
+
+#Display the dictionary.
+d
+
+# Delete the entry for “cake”.
+del d['cake']
+
+# Display the dictionary.
+d
+
+# Add an entry for “fruit” with “Mango” and display the dictionary.
+d['fruit'] = 'Mango'
+d
+
+# Display the dictionary keys.
+d.keys()
+
+# Display the dictionary values.
+d.values()
+
+# Display whether or not “cake” is a key in the dictionary
+# (i.e. False) (now).
+'cake' in d
+
+# Display whether or not “Mango” is a value in the dictionary
+#  (i.e. True).
+'Mango' in d.values()
+
+# Using the dictionary from item 1: Make a dictionary using
+# the same keys but with the number of ‘t’s in each value.
+
+# Create sets s2, s3 and s4 that contain numbers from zero through
+# twenty, divisible 2, 3 and 4.
+
+# Display the sets.
+# Display if s3 is a subset of s2 (False)
+# and if s4 is a subset of s2 (True).
+
+
+# Create a set with the letters in ‘Python’ and add ‘i’ to the set.
+
+# Create a frozenset with the letters in ‘marathon’
+# display the union and intersection of the two sets.
+
+
+
+
+
+
+
 
