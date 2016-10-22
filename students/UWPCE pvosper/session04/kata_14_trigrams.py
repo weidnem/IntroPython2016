c2005fcc36e7503fb5cbe472d64124b42a1b2596
pvosper <pvosper@mac.com>
2016-10-22 13:06:02 -0700

    make_sentance working (sorta). Lots of room for improvement

--- students/UWPCE pvosper/session04/kata_14_trigrams.py
+++ students/UWPCE pvosper/session04/kata_14_trigrams.py
@@ -6,6 +6,9 @@ Generate text based on trigrams within a source file.
 
 import random
 
+sentance = ""
+sentance_list = []
+
 # text_string = '''
 # Trigram analysis is very simple. Look at each set of three adjacent words in a 
 # document. Use the first two words of the set as a key, and remember the fact 
@@ -26,7 +29,7 @@ for i, entry in enumerate(text_list):
     temp_string = ''
     for char in entry:
         if char.isalpha():
-            temp_string = temp_string + char #.lower()
+            temp_string = temp_string + char.lower()
     text_list[i] = temp_string
 
 # In trigrams dictionary, each value is a list of lists
@@ -42,26 +45,25 @@ for i in range(len(text_list)-2):
 def pick_value(val_list):
     # random includes upper range value
     return val_list[random.randint(0, len(val_list) - 1)]
+    
+def make_sentance():
+    sentance = ""
+    seed = random.choice(list(trigrams.keys()))
+    sentance_list = seed.split()
+    for i in range(50):
+#     while trigrams[seed]:        
+        seed = sentance_list[-2] + " " + sentance_list[-1]
+        sentance_list.append(pick_value(trigrams[seed])[0])
+    for word in sentance_list:
+        sentance = sentance + " " + word
+    print("sentance: ", sentance)
+    return sentance
 
 if __name__ == '__main__':
     print('\n=== MAIN ===\n')
+    
+    make_sentance()
+    print(sentance)
 
-seed = random.choice(list(trigrams.keys()))
-print("seed = ", seed)
-print("trigrams[seed] = ", trigrams[seed])
-
-print("next word: ", pick_value(trigrams[seed])[0])
-
-s = seed + " " + pick_value(trigrams[seed])[0]
-
-print(s)
-
-sl = s.split()
-
-print(sl)
-
-next_seed = sl[-2] + " " + sl[-1]
-
-print(next_seed)
-
-print(trigrams[next_seed])
+# While true until lookup failer; then end sentence and start another
+# Create list of capitalized works - from capitals mid-sentence
