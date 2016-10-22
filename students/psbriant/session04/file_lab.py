dfeee54a8575a975a42230afff408a67573a78cd
pbriant <psbriant@q.com>
2016-10-22 16:19:02 -0700

    Created the methods copy_file and read_file and added docstrings for both.

--- students/psbriant/session04/file_lab.py
+++ students/psbriant/session04/file_lab.py
@@ -7,6 +7,10 @@ LAB: File Lab
 
 Description:
 This program explores handling files and parsing simple text.
+
+Questions:
+When should we use 'with' and why?
+To what degree should we handle exceptions?
 """
 # -------------------------------Import-----------------------------------------
 import pathlib
@@ -28,7 +32,27 @@ def print_path():
 def copy_file(file_name):
     """
     Takes in a file and copies it from the source to a destination.
+
+    Questions:
+    Correct method of specifying directory?
+    Intended way of copying file?
     """
+    lines = read_file(file_name)
+    outfile = open(file_name, 'wb')
+    for line in lines:
+        outfile.write(line)
+    outfile.close()
+
+
+def read_file(file_name):
+    """
+    Takes in a file as the only argument, reads it and returns each line as a
+    list of strings.
+    """
+    f = open(file_name, 'rb')
+    lines = f.readlines()
+    f.close()
+    return lines
 
 
 # ==============================================================================
@@ -38,7 +62,8 @@ def main():
     """
     Tests output.
     """
-    print_path()
+    # print_path()
+    # copy_file()
 
 if __name__ == '__main__':
     main()
