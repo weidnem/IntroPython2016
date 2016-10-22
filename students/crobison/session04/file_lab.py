7f738a4459a4ebaa962c263a1c9794668f4e722d
Robison, Charles <Charles.Robison@cdk.com>
2016-10-21 22:04:36 -0700

    add advanced topid - failures...

--- students/crobison/session04/file_lab.py
+++ students/crobison/session04/file_lab.py
@@ -1,6 +1,6 @@
 # Charles Robison
 # 2016.10.21
-# Dictionary and Set Lab
+# File Lab
 
 #!/usr/bin/env python
 import os
@@ -12,7 +12,32 @@ cwd = os.getcwd()
 for item in os.listdir(cwd):
     print(cwd + "/" + item)
 
-
-
 # write a program which copies a file from a source, to a
 # destination (without using shutil, or the OS copy command)
+file = open('file_lab01.txt', 'r')
+file_text = file.read()
+file_new = open('file_lab02.txt', 'w')
+file_new.write(file_text)
+file.close()
+file_new.close()
+
+# advanced: make it work for any size file: i.e. don’t read
+# the entire contents of the file into memory at once.
+file = open('file_lab01.txt', 'r')
+file_new = open('file_lab02.txt', 'w')
+file_text = file.readline()
+for line in file_text:
+    file_new.write(line)
+    line = file.readline()
+file.close()
+file_new.close()
+
+# not working correctl, second try:
+print('second try:')
+
+file_new = open('file_labe02.txt', 'w')
+with open('file_lab01.txt', 'r') as f:
+    for line in f:
+        file_text = f.readline()
+        file_new.write(line)
+file_new.close()
