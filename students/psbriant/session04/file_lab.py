28989765bec9565190e6ba6723edf2643c03805b
pbriant <psbriant@q.com>
2016-10-22 11:12:51 -0700

    Created file_lab.py, added basic organizational structure and updated header description.

---
+++ students/psbriant/session04/file_lab.py
@@ -0,0 +1,26 @@
+"""
+Name: Paul Briant
+Date: 10/24/16
+Class: Introduction to Python
+Session04
+LAB: File Lab
+
+Description:
+This program explores handling files and parsing simple text.
+"""
+# -------------------------------Import-----------------------------------------
+
+# -------------------------------Functions--------------------------------------
+
+
+# ==============================================================================
+
+
+def main():
+    """
+    Tests output.
+    """
+
+
+if __name__ == '__main__':
+    main()
