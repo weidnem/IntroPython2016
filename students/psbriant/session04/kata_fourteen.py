54e0b54d64cf4dadff893dad4e2fee6e3eb19112
pbriant <psbriant@q.com>
2016-10-23 18:39:58 -0700

    Added pseudo code to represent development process.

--- students/psbriant/session04/kata_fourteen.py
+++ students/psbriant/session04/kata_fourteen.py
@@ -11,6 +11,16 @@ Description:
 # -------------------------------Import-----------------------------------------
 
 # -------------------------------Functions--------------------------------------
+# Read file
+# Iterate through strings in file and place first three in a set.
+# Use the first two as a key in a dictionary and the third as the value.
+# Update the set so that it moves through the file one word at a time.
+# If key already exists, add new values.
+# Once entire file has been iterated through:
+# Choose random word pair as the start and use these to look up the next word.
+# Write to file.
+# Use second and third words to look up the next word.
+#
 
 # ==============================================================================
 
