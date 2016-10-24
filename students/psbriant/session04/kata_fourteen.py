92c271b55ccbb350baa84c86294847bb514a5e76
pbriant <psbriant@q.com>
2016-10-24 13:24:53 -0700

    created set generator method.

--- students/psbriant/session04/kata_fourteen.py
+++ students/psbriant/session04/kata_fourteen.py
@@ -11,7 +11,6 @@ Description:
 # -------------------------------Import-----------------------------------------
 
 # -------------------------------Functions--------------------------------------
-# Read file
 
 
 def read_file(file_name):
@@ -19,11 +18,28 @@ def read_file(file_name):
     Take in a file and return it as a string.
     """
     with open(file_name) as f:
-        strings = f.read()
-    return strings
+        string = f.read()
+    return string
 
 
-# Iterate through strings in file and place first three in a set.
+def set_generator(string):
+    """
+    Iterate through strings in file and place first three in a set.
+    """
+    str_list = string.split()
+    # Initialized set with first three words of file.
+    trigrams = set([str_list[0], str_list[1], str_list[2]])
+    tri_dict = {}
+
+    del str_list[0]
+    del str_list[1]
+    del str_list[2]
+
+    for item in str_list:
+        # trigrams = set([item, ])
+
+
+
 # Use the first two as a key in a dictionary and the third as the value.
 # Update the set so that it moves through the file one word at a time.
 # If key already exists, add new values.
