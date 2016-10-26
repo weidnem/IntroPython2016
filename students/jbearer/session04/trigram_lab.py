05fce495997d0bb7895e3c41fc5eae44685ad7e9
Jerry Bearer <jbearer@gmail.com>
2016-10-25 17:14:19 -0700

    Trigam, still work in progress.

--- students/jbearer/session04/trigram_lab.py
+++ students/jbearer/session04/trigram_lab.py
@@ -4,6 +4,8 @@ LAB: Trigrams
 October 20, 2016
 """
 
+import random
+
 def trigram_fun():
     with open('sherlock.txt','r') as file_in:
         my_book = file_in.read()
@@ -14,6 +16,7 @@ def trigram_fun():
     l_book = []
     l_book = my_book.split(' ')
 
+    bigram_list = list(zip(l_book,l_book[1:]))
     trigram_list = list(zip(l_book,l_book[1:],l_book[2:]))
 
     dict_list = {}
@@ -25,9 +28,36 @@ def trigram_fun():
         else:
             dict_list[tri_key] = [tri_value]
 
-    
-    for k, v in dict_list.items():
-        print('Keys:',k,'\n','Values:',v)
+
+    new_tri = []
+    myStr = trigram_list[2000][0] + ' ' + trigram_list[2000][1]
+    if myStr in dict_list.keys():
+        new_tri.append(trigram_list[2000][0])
+        new_tri.append(trigram_list[2000][1])
+        # print('myStr:',myStr)
+        # print('Values:',dict_list[myStr])
+        # print('new_tri:',new_tri)
+        rand_value = random.randrange(len(dict_list[myStr])-1)
+        # print('rand_value:',rand_value)
+        # print('Appending:',dict_list[myStr][rand_value])
+        new_tri.append(dict_list[myStr][rand_value])
+    else:
+        print('no')
+
+    for i in range(20):
+        myStr = new_tri[-2] + ' ' + new_tri[-1]
+        rand_value = random.randrange(len(dict_list[myStr])-1)
+        new_tri.append(dict_list[myStr][rand_value])
+
+        print('myStr:',myStr)
+        print('dict_list',dict_list[myStr])
+        print()
+        
+    print('new_tri:',new_tri)
+
+
+    # for k, v in dict_list.items():
+    #     print('Keys:',k,'\n','Values:',v)
 
 
 trigram_fun()
