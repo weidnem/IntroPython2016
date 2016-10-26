8514dfa49acb3e804a3f347bc8d40dc5bae5e1bb
abdiyo <abdirabinsha@gmail.com>
2016-10-26 05:06:33 -0700

    Modified ROT13 and slicing

--- students/Abdishu/session3/slicing.py
+++ students/Abdishu/session3/slicing.py
@@ -1,36 +1,44 @@
-'''
-Write some functions that:
-
 return a sequence with the first and last items exchanged.
 return a sequence with every other item removed
 return a sequence with the first and last 4 items removed, and every other item in between
 return a sequence reversed (just with slicing)
 return a sequence with the middle third, then last third, then the first third in the new order
-'''
+
+"""
 
 def first_last_exchanged():
-    list = input ("Enter a sequence of any charecters:  ")
-    
-    return list[-1] + list[1:-1] + list[0]
+    list = input ("Enter a sequence of any charecters so that the first and last item will be exchanged:  ")
 
-first_last_exchanged()
+    return list[-1:] + list[1:-1] + list[:1]
+
+
+print(first_last_exchanged())
 
 def remove_evryothr():
     seq = input("Please enter any sequence so that all even sequence will be removed :")
     return seq[::2]
 
-remove_evryothr()
+print(remove_evryothr())
 
 def first_lastfourRemoved():
     seq = input("Please enter any sequence so that the first and last 4 items will be removed and every other itme in between :")
     return seq[1:-4:2]
 
-first_lastfourRemoved
+print(first_lastfourRemoved())
 
 def reversed():
     seq = input("Please enter any sequence so that the sequence will be reversed! :")
     return seq[::-1]
 
-reversed()
+print(reversed())
 
+def midlelastfirstthird():
+        seq = input("Please enter any sequence so that  middle third, then last third, then the first third will be printed out!")
+
+       # total_len = len(seq)
+        #middle = len(seq)//2
+        total_third = len(seq)//3
+
+        return  seq[total_third:(2*total_third)] + seq[(total_third *2):] + seq[:total_third]
+print(midlelastfirstthird())
 
