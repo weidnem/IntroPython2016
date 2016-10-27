4c7f654885e9ffd3d95bde32129bf6e7c38abaf1
Jerry Bearer <jbearer@gmail.com>
2016-10-27 06:46:21 -0700

    Trigrams: will come back to this if time permits

--- students/jbearer/session04/trigram_lab.py
+++ students/jbearer/session04/trigram_lab.py
@@ -9,55 +9,53 @@ import random
 def trigram_fun():
     with open('sherlock.txt','r') as file_in:
         my_book = file_in.read()
-    file_in.close()
 
-    my_book = my_book.replace('\n', ' ')
+    my_book = my_book.replace('\n', ' ').strip()
 
-    l_book = []
-    l_book = my_book.split(' ')
+    l_book = my_book.split()
 
-    bigram_list = list(zip(l_book,l_book[1:]))
-    trigram_list = list(zip(l_book,l_book[1:],l_book[2:]))
+    trigram_list = zip(l_book,l_book[1:], l_book[2:])
 
+    # Builds the dictionary keys and values from the file.
     dict_list = {}
-    for i in range(len(trigram_list)):
-        tri_key = trigram_list[i][0] + ' ' + trigram_list[i][1]
-        tri_value = trigram_list[i][2]
-        if tri_key in dict_list:
-            dict_list[tri_key].append(tri_value)
-        else:
-            dict_list[tri_key] = [tri_value]
-
-
+    for first, second, third in trigram_list:
+        key = ' '.join((first, second))
+        dict_list.setdefault(key, []).append(third)
+    
+    # Starts building the list of words to use in the paragraph
     new_tri = []
-    myStr = trigram_list[2000][0] + ' ' + trigram_list[2000][1]
-    if myStr in dict_list.keys():
-        new_tri.append(trigram_list[2000][0])
-        new_tri.append(trigram_list[2000][1])
-        # print('myStr:',myStr)
-        # print('Values:',dict_list[myStr])
-        # print('new_tri:',new_tri)
-        rand_value = random.randrange(len(dict_list[myStr])-1)
-        # print('rand_value:',rand_value)
-        # print('Appending:',dict_list[myStr][rand_value])
-        new_tri.append(dict_list[myStr][rand_value])
-    else:
-        print('no')
+    myStr = list(dict_list.keys())[0]
+    new_tri.extend(myStr.split())
 
-    for i in range(20):
-        myStr = new_tri[-2] + ' ' + new_tri[-1]
-        rand_value = random.randrange(len(dict_list[myStr])-1)
-        new_tri.append(dict_list[myStr][rand_value])
+    for i in range(150):
+        myStr = ' '.join(new_tri[-2:])
 
-        print('myStr:',myStr)
-        print('dict_list',dict_list[myStr])
-        print()
-        
-    print('new_tri:',new_tri)
+        if myStr not in dict_list:
+            # Will add new key and value as empty list
+            print('myStr:', myStr)
+            print('no key in dict')
+        else:
+            new_tri.append(random.choice(dict_list[myStr]))
 
+    """ Start building something that looks like a paragraph of text
+        and strip unneeded characters from words.  Add capitalization
+        and a period at the end of each sentance.
+    """
 
-    # for k, v in dict_list.items():
-    #     print('Keys:',k,'\n','Values:',v)
+    exclude = set('./"!?\'')
 
+    for i in range(len(new_tri)):
+        myWord = new_tri[i]
+        myWord = ''.join(ch for ch in myWord if ch not in exclude)
+        if i % 13 == 0:
+            myWord = myWord.capitalize()
+            print(myWord, end=' ')
+        elif i % 13 == 12:
+            myWord = myWord.lower() + '.'
+            print(myWord, end=' ')
+        else:       
+            myWord = myWord.lower()
+            print(myWord, end=' ')
+    print() 
 
 trigram_fun()
