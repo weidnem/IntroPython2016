e5af6886b8b72bd041c4b752773e62e9fa9ba164
Nachiket Galande <nachiketgalande@Nachikets-MacBook-Pro.local>
2016-10-27 20:12:12 -0700

    Session05 class exercise commits

--- students/liverpoolforever/session04/file_lab.py
+++ students/liverpoolforever/session04/file_lab.py
@@ -1,6 +1,5 @@
 ''' File Lab 
 Write a little script that reads that file, and generates a list of all the languages that have been used.
-
 Extra credit: keep track of how many students specified each language.
 '''
 
@@ -8,8 +7,9 @@ import os
 from collections import defaultdict
 
 NAME_OF_DIR = '/Users/nachiketgalande/Documents/UWPython/IntroPython2016/Examples/Session01'
-# normal dict doesn't work for the counts
-lang_count = defaultdict(int) 
+# normal dict doesn't work for the counts - if the key is not there
+lang_count = {}
+# lang_count = defaultdict(int) 
 
 def main():
 	working_dir = os.getcwd()
@@ -27,8 +27,9 @@ def list_files(dir_name):
 
 def file_iterate(file_name):
 
-	file_hanlder = open(file_name,'r')
+	#  use 'with' to open files
 
+	file_hanlder = open(file_name,'r')
 	for line in file_hanlder:
 		# Split on comma to get students and langs
 		student_list = line.rstrip('\n').split(':')
@@ -37,14 +38,19 @@ def file_iterate(file_name):
 			#  split on comma to get languages
 			languages = student_list[1].split(',')
 			for lang in languages:
+				# strip() removes whitespace at the start and end not in between
+				lang = lang.strip().capitalize()
 				# can do this with default dict
-				lang_count[lang] += 1
+				if lang in lang_count.keys():
+					lang_count[lang] += 1
+				else:
+					lang_count[lang] = 1
 
 		else:
 			print("Bad entry in file" , student_list)
 
-	#  sort them
-	for k,v in sorted(lang_count.items(),reverse=True):
+	#  sort them - use a key for custom sort 
+	for k,v in sorted(lang_count.items(),):
 		print(k,v)
 
 	
