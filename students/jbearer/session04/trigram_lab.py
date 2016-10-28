a261e556e457237783a9d808d663b75aaae54f46
Jerry Bearer <jbearer@gmail.com>
2016-10-27 20:40:18 -0700

    Fixed conflict...I hope

--- students/jbearer/session04/trigram_lab.py
+++ students/jbearer/session04/trigram_lab.py
@@ -4,7 +4,6 @@ LAB: Trigrams
 October 20, 2016
 """
 
-<<<<<<< HEAD
 import random
 
 def trigram_fun():
@@ -58,32 +57,5 @@ def trigram_fun():
             myWord = myWord.lower()
             print(myWord, end=' ')
     print() 
-=======
-def trigram_fun():
-    with open('sherlock.txt','r') as file_in:
-        my_book = file_in.read()
-    file_in.close()
-
-    my_book = my_book.replace('\n', ' ')
-
-    l_book = []
-    l_book = my_book.split(' ')
-
-    trigram_list = list(zip(l_book,l_book[1:],l_book[2:]))
-
-    dict_list = {}
-    for i in range(len(trigram_list)):
-        tri_key = trigram_list[i][0] + ' ' + trigram_list[i][1]
-        tri_value = trigram_list[i][2]
-        if tri_key in dict_list:
-            dict_list[tri_key].append(tri_value)
-        else:
-            dict_list[tri_key] = [tri_value]
-
-    
-    for k, v in dict_list.items():
-        print('Keys:',k,'\n','Values:',v)
-
->>>>>>> b4af6a9862de4af8babd5c378cef795fdbba293b
 
 trigram_fun()
