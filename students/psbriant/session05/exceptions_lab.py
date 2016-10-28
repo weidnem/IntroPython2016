05d86cc580d37b73f54293040884d3487caf2608
pbriant <psbriant@q.com>
2016-10-28 14:42:46 -0700

    Added code to handel exceptions for the input function.

--- students/psbriant/session05/exceptions_lab.py
+++ students/psbriant/session05/exceptions_lab.py
@@ -6,12 +6,26 @@ Session05
 LAB: Exceptions
 
 Description:
-
+This program creates a wrapper to deal with the two possible exceptions for
+the input function.
 """
 
 # -------------------------------Import-----------------------------------------
 
 # -------------------------------Functions--------------------------------------
+
+
+def safe_input(question):
+    """
+    Takes in a multiword string and returns None if an exception is thrown.
+    """
+    try:
+        answer = input(question)
+        print(answer)
+    except KeyboardInterrupt:
+        return None
+    except EOFError:
+        return None
 
 # ==============================================================================
 
@@ -21,6 +35,7 @@ def main():
     Tests output.
     """
 
+    safe_input("What is your name? ")
 
 if __name__ == '__main__':
     main()
