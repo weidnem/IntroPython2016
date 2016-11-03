54a4b0758cfa5e7be4ca1653b6c756daeff9170b
Jerry Bearer <jbearer@gmail.com>
2016-11-02 19:24:53 -0700

    Week 6 Assignments

--- students/jbearer/session06/myfunctions.py
+++ students/jbearer/session06/myfunctions.py
@@ -8,10 +8,7 @@ def monkey_trouble(a_smile, b_smile):
     return a_smile is b_smile
 
 def squirrel_play(temp, is_summer):
-    if is_summer:
-        if temp >= 60 and temp <= 100:
-            return True
+    if temp >= 60 and temp <= 100 and is_summer or temp >= 60 and temp <= 90:
+        return True
     else:
-        if temp >= 60 and temp <= 90:
-            return True
-    return False
+        return False
