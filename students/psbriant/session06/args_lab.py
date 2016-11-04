94e9a70f3d31257456fdb57d8c86500440f007f5
pbriant <psbriant@q.com>
2016-11-04 14:34:30 -0700

    Added basic organizational structure.

---
+++ students/psbriant/session06/args_lab.py
@@ -0,0 +1,25 @@
+"""
+Name: Paul Briant
+Date: 11/15/16
+Class: Introduction to Python
+Session05
+LAB: Dictionaries
+
+Description:
+This module takes the dictionary lab from Session04 and incorporates
+comprehensions.
+"""
+
+# -------------------------------Functions--------------------------------------
+
+# ==============================================================================
+
+
+def main():
+    """
+    Tests output.
+    """
+
+
+if __name__ == '__main__':
+    main()
