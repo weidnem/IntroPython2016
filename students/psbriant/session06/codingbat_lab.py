e1f729d8de172e7f6593d0bc88198b15742c2acb
pbriant <psbriant@q.com>
2016-11-09 17:01:12 -0800

    codingbat_lab.py

---
+++ students/psbriant/session06/codingbat_lab.py
@@ -0,0 +1,28 @@
+"""
+Name: Paul Briant
+Date: 11/15/16
+Class: Introduction to Python
+Session06
+LAB: Codingbat Lab
+
+Description:
+
+"""
+
+
+# -------------------------------Imports----------------------------------------
+
+
+# -------------------------------Functions--------------------------------------
+
+
+# ==============================================================================
+
+
+def main():
+    """
+    Calls functions and produces output.
+    """
+
+if __name__ == '__main__':
+    main()
