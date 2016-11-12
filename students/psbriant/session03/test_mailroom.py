626ec0711ff32e8e67de872a73e3e4d0c5c1228b
pbriant <psbriant@q.com>
2016-11-12 14:17:35 -0800

    Created test module for mailroom.py and added basic organizational structure.

---
+++ students/psbriant/session03/test_mailroom.py
@@ -0,0 +1,23 @@
+"""
+Name: Paul Briant
+Date: 11/15/16
+Class: Introduction to Python
+Session: 03
+Assignment: Mailroom Tests
+
+Description:
+Tests for mailroom
+"""
+
+# -------------------------------Imports----------------------------------------
+import mailroom
+
+# -------------------------------Functions--------------------------------------
+
+# ==============================================================================
+def main():
+
+
+
+if __name__ == '__main__':
+    main()
