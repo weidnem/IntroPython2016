77dd94fba432b956f6ca205980dd52bf98eaa238
pvosper <pvosper@mac.com>
2016-11-12 07:08:57 -0800

    WIP Mailroom testing & refactor

---
+++ students/pvosper/session06/test_mailroom_2.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+# Test Mailroom_2
+
+'''
+Write a complete set of unit tests for your mailroom program.
+
+You will likely find that it is really hard to test without refactoring.
+
+This is Good!
+
+If code is hard to test – it probably should be refactored.
+
+'''
+
+from mailroom_2 import 
+
