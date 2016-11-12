69eef4c5c94258f82530b710fbcf37a16482a34e
pbriant <psbriant@q.com>
2016-11-11 17:14:08 -0800

    Created empty functions for each test case for the caught speeding function in the codingbat_lab.

--- students/psbriant/session06/test_codingbat_lab.py
+++ students/psbriant/session06/test_codingbat_lab.py
@@ -16,7 +16,52 @@ from codingbat_lab import caught_speeding, sum67
 
 
 # Tests for caught_speeding
+def test_60_false():
+    pass
 
+
+def test_65_false():
+    pass
+
+
+def test_65_true():
+    pass
+
+
+def test_80_false():
+    pass
+
+
+def test_85_false():
+    pass
+
+
+def test_85_true():
+    pass
+
+
+def test_70_false():
+    pass
+
+
+def test_75_false():
+    pass
+
+
+def test_75_true():
+    pass
+
+
+def test_40_false():
+    pass
+
+
+def test_40_true():
+    pass
+
+
+def test_90_false():
+    pass
 
 
 # Tests for sum_67
