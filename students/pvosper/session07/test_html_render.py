352d6156b6b8062eba3d763f93ae78b1d64cee12
pvosper <pvosper@mac.com>
2016-11-16 08:35:46 -0800

    WIP adding unit testing

---
+++ students/pvosper/session07/test_html_render.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python3
+
+'''
+Test code for html_render.py
+
+'''
+
+import io
+
+from html_render import Element
+
+def test_init():
+    e = Element()
+    
+    e = Element('this is some text')
+    
+def test_content():
+    # fixme: this tests internals!!!!
+    e = Element('this is some text')
+    
+    assert 'this is some text' in e.content
+    
+def test_append():
+    e = Element('this is some text')
+    
+    e.append('some more text')
+    
+    assert 'some more text' in e.content
+    
+def test_two_instances():
+    e = Element('this is some text')
+    e2 = Element('this is some other text')
+
+    e.append('some more text')
+    
+    assert 'some more text' not in e2.content
+
+def test_render():
+    outfile = io.StringIO()
+    e = Element('this is some text')
+    e.append('some more text')
+    e.render(outfile)
+    
+    outfile.seek(0)
+    file_contents = outfile.read()
+    
+    assert('this is some text') in file_contents
+    assert('some more text') in file_contents
+    
+    assert file_contents.startswith('<html>')
+    assert file_contents.strip().endswith('<\html>')
