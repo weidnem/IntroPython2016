e204e89c8805b19c000f9d3680ff4a0dbaa86d7e
pbriant <psbriant@q.com>
2016-11-16 22:07:37 -0800

    Created hmtl_renderer module with basic organizational structure.

---
+++ students/psbriant/session07/html_renderer.py
@@ -0,0 +1,27 @@
+"""
+Name: Paul Briant
+Date: 11/22/16
+Class: Introduction to Python
+Session07
+LAB: HTML Renderer
+
+Description:
+
+"""
+# -------------------------------Imports----------------------------------------
+
+
+# -------------------------------Functions--------------------------------------
+
+
+# ==============================================================================
+
+
+def main():
+    """
+    Calls functions and produces output.
+    """
+
+
+if __name__ == '__main__':
+    main()
