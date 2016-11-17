08bde344385499c03e450c7a1348784f8f600b06
pbriant <psbriant@q.com>
2016-11-17 15:02:28 -0800

    Created test file for html_renderer.

---
+++ students/psbriant/session07/tests_html_renderer.py
@@ -0,0 +1,15 @@
+"""
+Name: Paul Briant
+Date: 11/15/16
+Class: Introduction to Python
+Session: 07
+Assignment: HTML Renderer
+
+Description:
+Tests for HTML Renderer
+"""
+
+# -------------------------------Imports----------------------------------------
+import html_renderer.py
+
+# -------------------------------Functions--------------------------------------
