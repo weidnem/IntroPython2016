7fa9e840f7889ae9c540ff1f9bd8232f7178dced
Chris Barker <PythonCHB@gmail.com>
2016-11-20 20:28:53 -0800

    added another test for <br />

--- Solutions/Session07/step_5/test_html_render.py
+++ Solutions/Session07/step_5/test_html_render.py
@@ -311,6 +311,16 @@ def test_br():
     assert file_contents == "<br />"
 
 
+def test_br_in_p():
+    p = P("here is a small paragraph of text")
+    p.append(Br())
+    p.append("And here is some more text after a line break")
+
+    file_contents = render_result(p).split('\n')
+    print(file_contents)
+    assert file_contents[2].strip() == "<br />"
+
+
 def test_hr():
     hr = Hr(width=400)
     file_contents = render_result(hr)
