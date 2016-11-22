0e981cb6cc22a91169f04941d34115c3a231d6da
pvosper <pvosper@mac.com>
2016-11-22 07:40:24 -0800

    WIP: Step 4: Functional and clean [passed unit testing]

--- students/pvosper/session07/html_render.py
+++ students/pvosper/session07/html_render.py
@@ -11,17 +11,12 @@ Running tests using ipython:
     Creates -> test_html_output<step #>.html
 '''
 
-# def practice(**kwargs):
-#     for entry in kwargs.keys():
-#         print(entry, '=', kwargs[entry])
-# 
-# practice(id="TheList", style="line-height:200%")
-
 class Element:
     # Class names should normally use the CapWords convention
     # Class attributes are shared by all instances
     tag = 'html'    # Element doesn't really have a tag
     ind = ''
+    attr = ''
 
     # The __init__ method gets called when memory for the object is allocated
 
@@ -30,9 +25,12 @@ class Element:
         self.content = []
         if content:
             self.content.append(content)
-        # for entry in kwargs.keys():
-#             attr = (entry, '=', kwargs[entry]).replace(' ', '')
-#         tag = tag + 
+        # build single string for html attributes
+#         self.d = kwargs
+        self.l = []
+        for entry in kwargs.keys():
+            self.l.append(' ' + entry + '="' + kwargs[entry] + '"')
+        self.attr = ' '.join(self.l)
                 
     # Other Methods
 
@@ -43,7 +41,7 @@ class Element:
             self.content.append(str(content))
 
     def render(self, out_file, ind = ''):
-        out_file.write(self.ind + '<{}>\n'.format(self.tag))
+        out_file.write(self.ind + '<{}{}>\n'.format(self.tag, self.attr))
         for content in self.content:
             if hasattr(content, 'render'):
                 content.render(out_file, '')
@@ -54,12 +52,12 @@ class Element:
 class OneLineTag(Element):
 
     def render(self, out_file, ind = ''):
-        out_file.write(self.ind + '<{}>'.format(self.tag))
+        out_file.write(self.ind + '<{}{}>'.format(self.tag, self.attr))
         for content in self.content:
             if hasattr(content, 'render'):
                 content.render(out_file, "")
             else:
-                out_file.write(content)
+                out_file.write(' ' + content + ' ')
         out_file.write('</{}>\n'.format(self.tag))
 
 class Head(Element):
@@ -74,7 +72,7 @@ class Body(Element):
     tag = 'body'
     ind = '    '
 
-class P(OneLineTag):
+class P(Element):
     tag = 'p'
     ind = '        '
 
