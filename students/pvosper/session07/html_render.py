afe6b78fe7617e545d2cd91a3de15fe456bec1b6
Paul Vosper <pvosper@mac.com>
2016-11-22 19:09:36 -0800

    WIP: Working through Step 5, but always appreciate comments and suggestions (#112)
    * start
    
    * working version
    
    * functional (get it?)
    
    * tidy pass
    
    * initial working pass
    
    * second pass with functions, but there might be a better way
    
    * this is better
    
    * nasty brutish & short
    
    * even shorter
    
    * clean-up commented out methods
    
    * initial pass of all three functions
    
    * series.py semi-final pass
    
    * Slicing Lab - in-class assignment
    
    * initial setup
    
    * adding mailroom work
    
    * Donor report working
    
    * OK, got send mail working
    
    * Formatting reporting and adding averages
    
    * Everything works... a bit fragile, but works
    
    * Now with shebang :-)
    
    * Quick pass at rot13
    
    * initial commit
    
    * first section complete
    
    * finishing assignment
    
    * clean up whitespace at end of file
    
    * Progress! Built Trigram dictionary and created initial output seed
    
    * More work - strarting to print gibberish :-)
    
    * make_sentance working (sorta). Lots of room for improvement
    
    * referenced files
    
    * Better
    
    * Finished for now
    
    * Clean up source material for Kata 14
    
    * Empty file to start
    
    * Missed adding this one
    
    * Cleaning up some midlaid files
    
    * Playing catch up here
    
    * Adding notes
    
    * Initial pass at creating dictionary of students
    
    * Cleaning out crap
    
    * Cleaning up crap
    
    * Working through the file lab - still a ways to go
    
    * file_lab lunchtime work
    
    * conflict cleanup
    
    * File Lab for review
    
    * Initial file set-up for session05 homework
    
    * Exceptions Lab for review
    
    * Comprehensions Lab work - initial excercises
    
    * Comprehensions Lab finished
    
    * Test Lab for review, with support file codingbat
    
    * Test Lab for review, with support file codingbat
    
    * For review: whew! Really struggled with this one, so spent more time on it (probably over-thought it)
    
    * WIP Mailroom testing & refactor
    
    * WIP: Refactored and set up tests for Utilities, now need to finish
    
    * WIP initial commit
    
    * WIP adding unit testing
    
    * WIP run_html_render.py used for testing
    
    * WIP: Step 1 annotated
    
    * WIP: Step 2, indentation still not working right
    
    * WIP: Step 2 now has indent (I wasn't using self.ind), but it's not recursive
    
    * WIP: Step 2 better version of solution (still need to clean out comments)
    
    * WIP: Step 2: cleaned up
    
    * WIP: Fixed bug uncovered by unit test
    
    * WIP: Unit testing for html_render.py
    
    * WIP: Step 3: Functional & clean [passed unit testing]
    
    * WIP: Step 3: Cleanup on formatting for indentations; KWARGS expreiments for Step 4
    
    * WIP: Step 4: Functional and clean [passed unit testing]
    
    * WIP: Step 5: Functional, but no refactor

--- students/pvosper/session07/html_render.py
+++ students/pvosper/session07/html_render.py
@@ -1,1 +1,98 @@
-html_render.py
+#!/usr/bin/env python3
+
+'''
+Given formatted input, will write out a properly formatted html file
+
+Unit tests using PyTest:
+    py.test test_html_render.py
+
+Running tests using ipython:
+    run run_html_render.py
+    Creates -> test_html_output<step #>.html
+'''
+
+class Element:
+    # Class names should normally use the CapWords convention
+    # Class attributes are shared by all instances
+    tag = 'html'    # Element doesn't really have a tag
+    ind = ''
+
+    # The __init__ method gets called when memory for the object is allocated
+
+    def __init__(self, content=None, **kwargs):
+        # Instance attributes are unique to each instance
+        self.content = []
+        if content:
+            self.content.append(content)
+        # build single string for html attributes
+        self.l = []
+        for entry in kwargs.keys():
+            self.l.append(' ' + entry + '="' + kwargs[entry] + '"')
+        self.html_attr = ' '.join(self.l)
+                
+    # Other Methods
+
+    def append(self, content):
+        if hasattr(content, 'render'):
+            self.content.append(content)
+        else:
+            self.content.append(str(content))
+
+    def render(self, out_file, ind = ''):
+        out_file.write(self.ind + '<{}{}>\n'.format(self.tag, self.html_attr))
+        for content in self.content:
+            if hasattr(content, 'render'):
+                content.render(out_file, '')
+            else:
+                out_file.write(self.ind + '    ' + content + '\n')
+        out_file.write(self.ind + '</{}>\n'.format(self.tag))
+
+class OneLineTag(Element):
+
+    def render(self, out_file, ind = ''):
+        out_file.write(self.ind + '<{}{}>'.format(self.tag, self.html_attr))
+        for content in self.content:
+            if hasattr(content, 'render'):
+                content.render(out_file, '')
+            else:
+                out_file.write(' ' + content + ' ')
+        out_file.write('</{}>\n'.format(self.tag))
+        
+class SelfClosingTag(Element):
+
+    def render(self, out_file, ind = ''):
+        out_file.write(self.ind + '<{}{}>'.format(self.tag, self.html_attr))
+        for content in self.content:
+            if hasattr(content, 'render'):
+                content.render(out_file, '')
+            else:
+                out_file.write(' ' + content + ' ')
+        out_file.write('\n')
+
+class Head(Element):
+    tag = 'head'
+    ind = '    '
+
+class Html(Element):
+    tag = 'html'
+    ind = ''
+
+class Body(Element):
+    tag = 'body'
+    ind = '    '
+
+class P(Element):
+    tag = 'p'
+    ind = '        '
+
+class Title(OneLineTag):
+    tag = 'title'
+    ind = '        '
+
+class Hr(SelfClosingTag):
+    tag = 'hr'
+    ind = '        '
+
+class Br(SelfClosingTag):
+    tag = 'br'
+    ind = '        '
