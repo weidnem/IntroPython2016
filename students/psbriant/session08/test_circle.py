8e50e24bb8a3d9560eb3ad3fa1f77ae7b8aafd55
pbriant <psbriant@q.com>
2016-11-22 20:45:49 -0800

    Added tests for radius and diameter.

--- students/psbriant/session08/test_circle.py
+++ students/psbriant/session08/test_circle.py
@@ -11,5 +11,17 @@ Tests for Circle lab
 
 # -------------------------------Imports----------------------------------------
 
-
+from circle import Circle
 # -------------------------------Functions--------------------------------------
+
+
+def test_radius():
+    c = Circle(4)
+    assert c.radius == 4
+
+def test_diameter():
+    c = Circle(5)
+    assert c.diameter == 10
+
+def test_change_radius():
+    c = Circle
