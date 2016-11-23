a4a6f6cbeb3b66661182195f854845851426f9fc
pbriant <psbriant@q.com>
2016-11-22 20:40:38 -0800

    Created new files for circle lab.

---
+++ students/psbriant/session08/test_circle.py
@@ -0,0 +1,15 @@
+"""
+Name: Paul Briant
+Date: 11/22/16
+Class: Introduction to Python
+Session: 08
+Assignment: circle lab
+
+Description:
+Tests for Circle lab
+"""
+
+# -------------------------------Imports----------------------------------------
+
+
+# -------------------------------Functions--------------------------------------
