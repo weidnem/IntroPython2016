25deca5841de48060f3f61f28a3ed21968b55485
Jerry Bearer <jbearer@gmail.com>
2016-11-26 12:02:40 -0800

    Cirlce work from class

---
+++ students/jbearer/session08/test_circle.py
@@ -0,0 +1,28 @@
+"""
+Test for circle class
+"""
+import math
+from circle import Circle
+
+def test_radius():
+    c = Circle(4)
+    assert c.radius == 4
+
+def test_diameter():
+    c = Circle(5)
+    assert c.diameter  == 10
+
+def test_change_radius():
+    c = Circle(5)
+    c.radius = 2
+    assert c.diameter == 4
+
+def test_change_diameter():
+    c = Circle(5)
+    c.diameter = 12
+    assert c.diameter == 12
+    assert c.radius == 6
+
+def test_area():
+    c = Circle(4)
+    assert c.area == math.pi * 4 ** 2
