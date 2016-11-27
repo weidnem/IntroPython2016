2b766eca6e2e44719e7a599e2f4c282fb29d2146
pvosper <pvosper@mac.com>
2016-11-27 06:33:40 -0800

    WIP: Step 6 unit tests

--- students/pvosper/session07/test_html_render.py
+++ students/pvosper/session07/test_html_render.py
@@ -7,7 +7,7 @@ Test code for html_render.py
 
 import io
 
-from html_render import Element, Html, Body, P
+from html_render import Element, Html, Body, P, A
 
 def test_init():
     e = Element()
@@ -50,4 +50,27 @@ def test_render():
     assert file_contents.startswith('<html>')
     assert file_contents.strip().endswith('</html>')
 
+def test_link():
+    outfile = io.StringIO()
+    a = A("http://google.com", "link")
+    # assert '<a href="http://google.com">link</a>' == a.content
+
+    a.render(outfile)
+    
+    outfile.seek(0)
+    file_contents = outfile.read()
+    
+    print(file_contents)
+
+    assert file_contents.startswith('<a href="')
+    assert file_contents.strip().endswith('</a>')
+    
+
 # test Html, Body, P
+# self.html_attr
+# self.html_attr = ' '.join
+# self.tag_open = '<{}{}>\n
+# self.tag_open_single_line
+# self.tag_close = '</{}>\n
+# OneLineTag
+# SelfClosingTag
