cc7b6f3f8e9365199a30289f8cb7b1d182313541
Paul Vosper <pvosper@mac.com>
2016-11-27 22:12:14 -0800

    FOR REVIEW: html_render.py (#116)
    * start
    
    * working version
    
    * functional (get it?)
    
    * tidy pass
    
    * initial working pass
    
    * second pass with functions, but there might be a better way
    
    * this is better
    
    * nasty brutish & short
    
    * even shorter
    
    * clean-up commented out methods
    
    * initial pass of all three functions
    
    * series.py semi-final pass
    
    * Slicing Lab - in-class assignment
    
    * initial setup
    
    * adding mailroom work
    
    * Donor report working
    
    * OK, got send mail working
    
    * Formatting reporting and adding averages
    
    * Everything works... a bit fragile, but works
    
    * Now with shebang :-)
    
    * Quick pass at rot13
    
    * initial commit
    
    * first section complete
    
    * finishing assignment
    
    * clean up whitespace at end of file
    
    * Progress! Built Trigram dictionary and created initial output seed
    
    * More work - strarting to print gibberish :-)
    
    * make_sentance working (sorta). Lots of room for improvement
    
    * referenced files
    
    * Better
    
    * Finished for now
    
    * Clean up source material for Kata 14
    
    * Empty file to start
    
    * Missed adding this one
    
    * Cleaning up some midlaid files
    
    * Playing catch up here
    
    * Adding notes
    
    * Initial pass at creating dictionary of students
    
    * Cleaning out crap
    
    * Cleaning up crap
    
    * Working through the file lab - still a ways to go
    
    * file_lab lunchtime work
    
    * conflict cleanup
    
    * File Lab for review
    
    * Initial file set-up for session05 homework
    
    * Exceptions Lab for review
    
    * Comprehensions Lab work - initial excercises
    
    * Comprehensions Lab finished
    
    * Test Lab for review, with support file codingbat
    
    * Test Lab for review, with support file codingbat
    
    * For review: whew! Really struggled with this one, so spent more time on it (probably over-thought it)
    
    * WIP Mailroom testing & refactor
    
    * WIP: Refactored and set up tests for Utilities, now need to finish
    
    * WIP initial commit
    
    * WIP adding unit testing
    
    * WIP run_html_render.py used for testing
    
    * WIP: Step 1 annotated
    
    * WIP: Step 2, indentation still not working right
    
    * WIP: Step 2 now has indent (I wasn't using self.ind), but it's not recursive
    
    * WIP: Step 2 better version of solution (still need to clean out comments)
    
    * WIP: Step 2: cleaned up
    
    * WIP: Fixed bug uncovered by unit test
    
    * WIP: Unit testing for html_render.py
    
    * WIP: Step 3: Functional & clean [passed unit testing]
    
    * WIP: Step 3: Cleanup on formatting for indentations; KWARGS expreiments for Step 4
    
    * WIP: Step 4: Functional and clean [passed unit testing]
    
    * WIP: Step 5: Functional, but no refactor
    
    * WIP: Start
    
    * stashing local changes to pull code reviewed version
    
    * WIP: AFAICT this does NOT include code review changes, though I did the pull from the upstream master
    
    * WIP: Step 5 complete - finished work started in class
    
    * WIP: Step 6: Functional; includes unit test, but I want to review later and clean up a bit
    
    * WIP: Getting ready to refactor class A to subclass OneLineTag
    
    * WIP: Step 6: better version
    
    * WIP: Step 6 unit tests
    
    * WIP: Step 7: Functional, still not using supers
    
    * WIP: Step 7
    
    * WIP: Step 8: 1/2 done but I ran out of time - I'll set up the pull request and try for some overtime
    
    * Step 8: Fixes & Additions: fixed SelfClosingTag to render '/>' by adding 'self.tag_self_close'; Meta class working but indentation broken
    
    * Step 8: fixed indent bug for Meta and Html classes
    
    * Step 8: fixed bug with exra line feed after SelfClosingTag
    
    * Catching up
    
    * Aligning changes
    
    * Clean up on aisle 3

--- students/pvosper/session07/run_html_render.py
+++ students/pvosper/session07/run_html_render.py
@@ -11,7 +11,6 @@ from io import StringIO
 
 # importing the html_rendering code with a short name for easy typing.
 import html_render as hr
-
 # reloading in case you are running this in iPython
 #  -- we want to make sure the latest version is used
 import importlib
@@ -40,14 +39,18 @@ def render_page(page, filename):
 
 # Step 1
 #########
+# 
+# page = hr.Element()
+# 
+# page.append("Here is a paragraph of text -- there could be more of them, "
+#             "but this is enough  to show that we can do some text")
+# 
+# page.append("And here is another piece of text -- you should be able to add any number")
+# 
+# render_page(page, "test_html_output1.html")
 
-page = hr.Element()
-
-page.append("Here is a paragraph of text -- there could be more of them, but this is enough to show that we can do some text")
-
-page.append("And here is another piece of text -- you should be able to add any number")
-
-render_page(page, "test_html_output1.html")
+# The rest of the steps have been commented out.
+#  Uncomment them a you move along with the assignment.
 
 # ## Step 2
 # ##########
@@ -56,7 +59,8 @@ render_page(page, "test_html_output1.html")
 
 # body = hr.Body()
 
-# body.append(hr.P("Here is a paragraph of text -- there could be more of them, but this is enough  to show that we can do some text"))
+# body.append(hr.P("Here is a paragraph of text -- there could be more of them, "
+#                  "but this is enough  to show that we can do some text"))
 
 # body.append(hr.P("And here is another piece of text -- you should be able to add any number"))
 
@@ -76,7 +80,8 @@ render_page(page, "test_html_output1.html")
 
 # body = hr.Body()
 
-# body.append(hr.P("Here is a paragraph of text -- there could be more of them, but this is enough  to show that we can do some text"))
+# body.append(hr.P("Here is a paragraph of text -- there could be more of them, "
+#                  "but this is enough  to show that we can do some text"))
 # body.append(hr.P("And here is another piece of text -- you should be able to add any number"))
 
 # page.append(body)
@@ -95,7 +100,8 @@ render_page(page, "test_html_output1.html")
 
 # body = hr.Body()
 
-# body.append(hr.P("Here is a paragraph of text -- there could be more of them, but this is enough  to show that we can do some text",
+# body.append(hr.P("Here is a paragraph of text -- there could be more of them, "
+#                  "but this is enough  to show that we can do some text",
 #               style="text-align: center; font-style: oblique;"))
 
 # page.append(body)
@@ -114,7 +120,8 @@ render_page(page, "test_html_output1.html")
 
 # body = hr.Body()
 
-# body.append(hr.P("Here is a paragraph of text -- there could be more of them, but this is enough  to show that we can do some text",
+# body.append(hr.P("Here is a paragraph of text -- there could be more of them, "
+#                  "but this is enough  to show that we can do some text",
 #               style="text-align: center; font-style: oblique;"))
 
 # body.append(hr.Hr())
@@ -135,7 +142,8 @@ render_page(page, "test_html_output1.html")
 
 # body = hr.Body()
 
-# body.append(hr.P("Here is a paragraph of text -- there could be more of them, but this is enough  to show that we can do some text",
+# body.append(hr.P("Here is a paragraph of text -- there could be more of them, "
+#                  "but this is enough  to show that we can do some text",
 #               style="text-align: center; font-style: oblique;"))
 
 # body.append(hr.Hr())
@@ -162,7 +170,8 @@ render_page(page, "test_html_output1.html")
 
 # body.append( hr.H(2, "PythonClass - Class 6 example") )
 
-# body.append(hr.P("Here is a paragraph of text -- there could be more of them, but this is enough  to show that we can do some text",
+# body.append(hr.P("Here is a paragraph of text -- there could be more of them, "
+#                  "but this is enough  to show that we can do some text",
 #               style="text-align: center; font-style: oblique;"))
 
 # body.append(hr.Hr())
@@ -188,38 +197,39 @@ render_page(page, "test_html_output1.html")
 # # Step 8
 # ########
 
-# page = hr.Html()
+page = hr.Html()
 
 
-# head = hr.Head()
-# head.append( hr.Meta(charset="UTF-8") )
-# head.append(hr.Title("PythonClass = Revision 1087:"))
+head = hr.Head()
+head.append( hr.Meta(charset="UTF-8") )
+head.append(hr.Title("PythonClass = Revision 1087:"))
 
-# page.append(head)
+page.append(head)
 
-# body = hr.Body()
+body = hr.Body()
 
-# body.append( hr.H(2, "PythonClass - Class 6 example") )
+body.append( hr.H(2, "PythonClass - Class 6 example") )
 
-# body.append(hr.P("Here is a paragraph of text -- there could be more of them, but this is enough  to show that we can do some text",
-#                  style="text-align: center; font-style: oblique;"))
+body.append(hr.P("Here is a paragraph of text -- there could be more of them, "
+                 "but this is enough  to show that we can do some text",
+                 style="text-align: center; font-style: oblique;"))
 
-# body.append(hr.Hr())
+body.append(hr.Hr())
 
-# list = hr.Ul(id="TheList", style="line-height:200%")
+list = hr.Ul(id="TheList", style="line-height:200%")
 
-# list.append( hr.Li("The first item in a list") )
-# list.append( hr.Li("This is the second item", style="color: red") )
+list.append( hr.Li("The first item in a list") )
+list.append( hr.Li("This is the second item", style="color: red") )
 
-# item = hr.Li()
-# item.append("And this is a ")
-# item.append( hr.A("http://google.com", "link") )
-# item.append("to google")
+item = hr.Li()
+item.append("And this is a ")
+item.append( hr.A("http://google.com", "link") )
+item.append("to google")
 
-# list.append(item)
+list.append(item)
 
-# body.append(list)
+body.append(list)
 
-# page.append(body)
+page.append(body)
 
-# render_page(page, "test_html_output8.html")
+render_page(page, "test_html_output8.html")
