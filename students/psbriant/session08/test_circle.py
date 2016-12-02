fd89c045975d8773416f8acaca96bbdccdda5610
pbriant <psbriant@q.com>
2016-12-02 13:24:51 -0800

    Add Docstrings for tests.

--- students/psbriant/session08/test_circle.py
+++ students/psbriant/session08/test_circle.py
@@ -16,22 +16,34 @@ from circle import Circle
 
 
 def test_radius():
+    """
+    Test radius attribute
+    """
     c = Circle(4)
     assert c.radius == 4
 
 
 def test_diameter():
+    """
+    Test diameter attribute
+    """
     c = Circle(5)
     assert c.diameter == 10
 
 
 def test_change_radius():
+    """
+    Test ability for class to alter radius
+    """
     c = Circle(5)
     c.radius = 2
     assert c.diameter == 4
 
 
 def test_change_diameter():
+    """
+    Test ability for class to alter diameter
+    """
     c = Circle(5)
     c.diameter = 12
     assert c.diameter == 12
