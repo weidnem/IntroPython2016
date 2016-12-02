37bd2046102e6e787bc54da53c1337768e708f2f
pbriant <psbriant@q.com>
2016-12-01 21:51:36 -0800

    Add class method for diameter.

--- students/psbriant/session08/circle.py
+++ students/psbriant/session08/circle.py
@@ -7,10 +7,21 @@ class Circle:
 
         self.diameter = radius * 2
 
+        @classmethod
+        def from_diameter(cls, diameter):
+            self = cls(diameter / 2)
+            return self
+
+        def __str__(self):
+            return "A circle object with radius: {}".format(self.radius)
+
+        def __repr__(self):
+            return "Circle({})".format(self.radius)
+
         @property
         def diameter(self):
             return self.radius * 2
+
         @diameter.setter
         def diameter(self, value):
             self.radius = value / 2
-            
