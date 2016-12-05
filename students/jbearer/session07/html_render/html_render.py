c1588fe0b84c085ef2e1f76215a00fc9b1c30498
Jerry Bearer <jbearer@gmail.com>
2016-12-05 08:07:34 -0800

    Fixing conflict

--- students/jbearer/session07/html_render/html_render.py
+++ students/jbearer/session07/html_render/html_render.py
@@ -1,68 +1,22 @@
 #!/usr/bin/env python3
 
-class TextWrapper:
-    """
-    A simple wrapper that creates a class with a render method
-    for just text
-    This allows the Element classes to render either Element objects or
-    plain text
-    """
-    def __init__(self, text):
-        self.text = text
-
-    def render(self, file_out, current_ind=""):
-        file_out.write(current_ind + self.text)
 
 class Element:
-    
     tag = 'html'
-    indent = "    "
-
-    def __init__(self, content=None, **kwargs):
-        self.attributes = kwargs
+    def __init__(self, content=None):
         self.content = []
         if content:
-            self.append(content)
+            self.content.append(content)
 
     def append(self, content):
-        if hasattr(content, 'render'):
-            self.content.append(content)
-        else:
-            self.content.append(TextWrapper(str(content)))
+        self.content.append(content)
 
     def render(self, out_file, ind=''):
-        open_tag, close_tag = self.make_tags()
-        out_file.write(ind + open_tag + '\n')
+        out_file.write("<{}>\n".format(self.tag))
         for stuff in self.content:
-            stuff.render(out_file, ind + self.indent)
-            out_file.write('\n')
-        out_file.write(ind + close_tag)
+            out_file.write(stuff+"\n")
+        out_file.write("</{}>\n".format(self.tag))
 
-
-   
-    def make_tags(self):
-        """
-        create the tags
-        -- in a separate method so different subclass's render methods can use it
-        """
-        attrs = " ".join(['{}="{}"'.format(key, val) for key, val in self.attributes.items()])
-        if attrs.strip():
-            open_tag = "<{} {}>".format(self.tag, attrs.strip())
-        else:
-            open_tag = "<{}>".format(self.tag)
-        close_tag = "</{}>".format(self.tag)
-
-        return open_tag, close_tag
-
-class OneLineTag(Element):
-    # note: by re-writting the render
-    def render(self, out_file, ind=""):
-        # there is some repition here -- maybe factor that out?
-        open_tag, close_tag = self.make_tags()
-        out_file.write(ind + open_tag)
-        for stuff in self.content:
-            stuff.render(out_file)
-        out_file.write(close_tag)
 
 class Html(Element):
     tag = 'html'
@@ -73,68 +27,36 @@ class Body(Element):
 class P(Element):
     tag = 'p'
 
-class Head(Element):
-    tag = 'head'
-
-class Title(OneLineTag):
-    tag = 'title'
-
-class SelfClosingTag(Element):
-    """
-    base class for tags that have no content
-    """
-    def render(self, out_file, ind=""):
-        # there is some repition here -- maybe factor that out?
-        open_tag, _ = self.make_tags()
-        # make it a self cloding tag by adding the /
-        out_file.write(ind + open_tag.replace(">", " />"))
-
-class Hr(SelfClosingTag):
-    tag = 'hr'
-
-class Br(SelfClosingTag):
-    tag = 'br'
-
-class A(OneLineTag):
-    """
-    anchor element
-    """
-    tag = "a"
-
-    def __init__(self, link, content, **kwargs):
-        kwargs['href'] = link
-        super().__init__(content, **kwargs)
-        # this could also be direct:
-        # Element.__init__(self, content, **kwargs)
-
-class Ul(Element):
-    """
-    unordered list
-    """
-    tag = "ul"
 
 
-class Li(Element):
-    """
-    list element
-    """
-    tag = "li"
 
 
-class H(OneLineTag):
-    """
-    section head
-    """
-    tag = "H"
 
-    def __init__(self, level, content=None, **kwargs):
-        self.tag = "h" + str(int(level))
-        super().__init__(content, **kwargs)
 
-class Meta(SelfClosingTag):
-    """
-    metadata tag
-    """
-    tag = "meta"
+# class Element:
+    
+#     tag = 'aaa'
 
-# print(help(Body))
+#     def __init__(self, content=None):
+#         self.content = []
+#         if content:
+#             self.content.append(content)
+
+#     def append(self, content):
+#         self.content.append(content)
+
+#     def render(self, out_file, ind=''):
+#         if self.tag == 'html':
+#             out_file.write('<html>\n')
+#         for stuff in self.content:
+#             out_file.write(stuff + '\n')
+        
+#         # if self.tag == 'body':
+#         #     out_file.write('<body>\n')
+#         # for stuff in self.content:
+#         #     out_file.write(stuff + '\n')
+#         # if self.tag == 'body':
+#         #     out_file.write('</body>\n')
+
+#         if self.tag == 'html':
+#             out_file.write('</html>\n')
