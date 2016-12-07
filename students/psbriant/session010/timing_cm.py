515b3b5f0d1a973134e91ee56c7484b34eab7cd8
pbriant <psbriant@q.com>
2016-12-06 20:56:39 -0800

    Add init method.

--- students/psbriant/session010/timing_cm.py
+++ students/psbriant/session010/timing_cm.py
@@ -8,9 +8,12 @@ import time
 
 
 class Timer:
+    def __init__(self, outfile):
+        self.outfile = outfile
 
     def __enter__(self):
         self.start = time.time()
 
     def __exit__(self, exc_type, exc_val, exc_tb):
         print("elapsed time:", time.time() - self.start)
+        self.outfile.write("elapsed time:{} seconds".format(time.time))
