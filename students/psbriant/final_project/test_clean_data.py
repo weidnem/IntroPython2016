650d6c72054ac5cd406af8a37e45d0349cf521dc
pbriant <psbriant@q.com>
2016-12-07 21:11:19 -0800

    Add organizational structure and import calls to clean_data.py and io.

---
+++ students/psbriant/final_project/test_clean_data.py
@@ -0,0 +1,12 @@
+"""
+Name: Paul Briant
+Date: 12/11/16
+Class: Introduction to Python
+Assignment: Final Project
+
+Description:
+Tests for Final Project
+"""
+
+import clean_data.py as cd
+import io
