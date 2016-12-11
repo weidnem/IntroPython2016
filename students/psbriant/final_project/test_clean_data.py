f321b8e612690b6c076c76e3aa403df6337fc2b1
pbriant <psbriant@q.com>
2016-12-11 10:43:48 -0800

    Added test functions to test clean_data module.

--- students/psbriant/final_project/test_clean_data.py
+++ students/psbriant/final_project/test_clean_data.py
@@ -9,4 +9,30 @@ Tests for Final Project
 """
 
 import clean_data.py as cd
+import pandas
 import io
+
+
+
+def test_clean():
+    """
+
+    """
+
+
+def test_rename_columns():
+    """
+
+    """
+
+
+def test_find_low_water_use():
+    """
+
+    """
+
+
+def test_plot_zipcode():
+    """
+
+    """
